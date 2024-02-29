221ac382bc615ab59092f379498e014ea11eddac
VictorCancino <juanvictorcancino@gmail.com>
2024-02-29 16:56:43 -0600

    Se crearón operaciones básicas con python

---
+++ 10_por_ciento/operaciones.py
@@ -0,0 +1,61 @@
+
+#Operación suma:
+a = 2
+b = 3
+
+suma = a + b
+
+print(suma)
+
+#Operación resta:
+
+r_a = 8
+r_b = 10
+
+resta = r_b - r_a
+
+print(resta)
+
+#Operación multiplicación:
+
+m_a = 2
+m_b = 12
+
+multiplicacion = m_a * m_b
+
+print(multiplicacion)
+
+#Operación división: 
+
+d_a = 7
+d_b = 3
+
+division = d_a / d_b
+
+print(division)
+
+#Operación entera
+
+de_a = 7
+de_b = 3
+
+division_entera = de_a // de_b
+
+print(division_entera)
+
+#Operación de modulo (residuo de división)
+
+m_a = 8
+m_b = 3
+
+modulo = m_a % m_b
+
+print(modulo)
+
+#Operación de exponente 
+
+e_a = 4
+
+exponente = e_a**3
+
+print(exponente)
