356f5ce2e467b34a23502788e888f1d52d5916ae
VictorCancino <juanvictorcancino@gmail.com>
2024-03-01 15:27:39 -0600

    Se agregó el planteamiento del problema al ejercicio 'almacen.py'

---
+++ 20_por_ciento/almacen.py
@@ -0,0 +1,9 @@
+"""
+Planteamiento del problema:
+
+Suponagmos que trabajas en un negocio donde eres el responsable del almacen. Tu trabajo consiste en recibir los diferentes articulos que llegan al negocio y colocarlo en el anaquel correspondiente.
+
+Para ello debes verificar el particulo en la barra de recepción y después ingresar al sistema e ingresar el tipo de articulo que estas recibiendo, una vez que lo consultas con el sistema, este te devuelve el numero de anaquel en el cuál debes colocar el articulo, así como el numero de fila y columna.
+
+Después debes llevar el articulo al anaquel y confirmar en el sistema que el articulo a sido colocado correctamente. 
+"""
