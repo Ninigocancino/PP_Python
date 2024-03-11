f8e2a9373b7b7b7d601c8a71e0e52a8087678ab0
VictorCancino <juanvictorcancino@gmail.com>
2024-03-10 20:12:40 -0600

    Se agregarón las instrucciones para realizar el programa 'reservacion'

---
+++ 20_por_ciento/reservacion.py
@@ -0,0 +1,18 @@
+"""
+Supongamos que el gerente de un restaurante muy visitado en tu ciudad te ha contratado para crear un programa que les ayude a gestionar las númerosas reservaciones que realizan en el restaurante.
+
+El gerente te explica que local tiene capacidad maxima para 55 personas disribuidas de la siguiente manera:
+
+28 personas acomodadas en mesas de 4 personas, 12 personas acomodadas en mesas de dos personas y 5 personas  en la barra sentadas individualmente.
+
+como el restaurante es tan concurrido las reservaciones son necesarias para poder ingresar, así que los comensales deben ingresar al sitio web del restuarante buscar la diponibilidad de la fecha agrear sus datos de registros, si la fecha no esta disponible los comensales tienen la opción de elegir una ficha de espera, si alguien que reservó en el horario deseado no se presenta en el periodo de toleracia de la reservación el personal del restaurante deja pasar al comensal con el turno correspondiente. solo se pueden entregar 15 turnos o fichas que no pueden superar el 20 % de ocupación del local.
+
+Crea un programa que permita realizar una reservación en un restaurante y se capaz de:
+Interactuar con el usuario.
+Verificar si la fecha y horario de reservación esta disponible.
+Imprimir en pantalla la confirmación del registro de la reservación
+Dar la opción de elegir un turno o ficha
+imprimir el numero de turno que se le asigna al usuario
+Además debe imprimir las reglas que apliquen al turno como a la reservación.
+
+"""
