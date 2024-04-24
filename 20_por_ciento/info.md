a4444f0c1a08981e98c23f9c721a888c8e56ef3d
VictorCancino <juanvictorcancino@gmail.com>
2024-04-24 10:06:24 -0600

    Se agregó el fichero 'info.md' del directorio '20_por_ciento' y se agregarón las instrucciones de está etapa del proyecto

---
+++ 20_por_ciento/info.md
@@ -0,0 +1,12 @@
+"20_por_ciento" contiene ejercicios que permiten adquirir un dominio del lenguaje Python en un 20%. Debido a la sencillez de esta instancia, se han ubicado los ejercicios en archivos ".py" y se ha omitido la creación de archivos "README.md". En cambio, las instrucciones o información contextual se ubican dentro del propio archivo en forma de comentarios de código.
+
+Contemplamos que para adquirir un dominio del lenguaje Python debes ser capaz de realizar las siguientes acciones:
+
+Utilizar estructuras de control como bucles (for, while) y condicionales (if, else).
+Manipular listas y acceder a sus elementos.
+Utilizar la función range() para generar secuencias numéricas.
+
+Y alcanzar los siguientes hitos:
+
+realizar 5 ejercicios de cada acción 
+realizar 3 ejercicios pequeños donde puedas aplicar lo realizado en etapas anteriores
--- 60_por_ciento/FormEd/main.py
+++ 60_por_ciento/FormEd/main.py
@@ -1,18 +1,24 @@
 import csv
 import os 
 
-print((" ") * 48, "FormEd")
-print("")
-print((" ") * 32, "Aparta tu lugar en nuestro próximo Curso:")
-print((" ") * 26, "Curso Básico de Transformación Digital para Negocios")
-print("")
-print((" ") * 28, "¿Qué quieres hacer? (escribe la opción deseada)")
-print((" ") * 27, "[INSCRIBIRME]", (" ") * 5, "[SALIR]", (" ") * 5, "[INSTRUCCIONES]")
+def imprimir_menu():
+     
+     #Función que imprime el menú de opciones 
+     print((" ") * 48, "FormEd")
+     print("")
+     print((" ") * 32, "Aparta tu lugar en nuestro próximo Curso:")
+     print((" ") * 26, "Curso Básico de Transformación Digital para Negocios")
+     print("")
+     print((" ") * 28, "¿Qué quieres hacer? (escribe la opción deseada)")
+     print((" ") * 27, "[INSCRIBIRME]", (" ") * 5, "[SALIR]", (" ") * 5, "[INSTRUCCIONES]")
+     print(" ")
 
-print(" ")
 eleccion = input("Ingresa tu eleccion: ").upper()
 if eleccion == "INSCRIBIRME":
-    
+
+
+def crear_cuenta():
+
     #Creación de la cuenta del usuario
 
     comprobador = input("¿Listo para apartar tu lugar? (Sí), (No): ")
@@ -74,76 +80,76 @@ if eleccion == "INSCRIBIRME":
 
                 break
 
-            print("")
-            print("Ahora apartemos tu lugar en el curso")
-            print("")
+def elegir_curso():
+    print("")
+    print("Ahora apartemos tu lugar en el curso")
+    print("")
 
-            print("Fechas disponibles: ")
-            print(" Grupo: 1, 10 de marzo de 2024")
-            print(" Grupo: 2, 20 de marzo de 2024")
-            print(" Grupo: 3, 05 de abril de 2024")
-            print("")
+    print("Fechas disponibles: ")
+    print(" Grupo: 1, 10 de marzo de 2024")
+    print(" Grupo: 2, 20 de marzo de 2024")
+    print(" Grupo: 3, 05 de abril de 2024")
+    print("")
 
-            fecha_elegida = int(input("Ingresa el número del grupo al que deseas inscribite: "))
+    fecha_elegida = int(input("Ingresa el número del grupo al que deseas inscribite: "))
 
-            print("")
-            print("Ahora elige el horario deseado")
-            print("")
+    print("")
+    print("Ahora elige el horario deseado")
+    print("")
 
-            print(" A, 08:00 am -> 10:00 am")
-            print(" B, 12:00 pm -> 02:00 pm")
-            print(" C, 06:00 pm -> 08:00 pm")
-            print("")
+    print(" A, 08:00 am -> 10:00 am")
+    print(" B, 12:00 pm -> 02:00 pm")
+    print(" C, 06:00 pm -> 08:00 pm")
+    print("")
 
-            horario_elegido = input("Ingresa el número del horario en el que deseas inscribirte: ")
+    horario_elegido = input("Ingresa el número del horario en el que deseas inscribirte: ")
 
-            print("")
+    print("")
 
-            data_path_2 = "Datos_cursos"          
+    data_path_2 = "Datos_cursos" 
 
-            if not os.path.exists(data_path_2):
-                os.makedirs(data_path_2)
+    if not os.path.exists(data_path_2):
+        os.makedirs(data_path_2)
 
-            if fecha_elegida == 1 and horario_elegido == "A":
-                name_file = "G1A_Curso_01_inscripicones.csv"
+        if fecha_elegida == 1 and horario_elegido == "A":
+            name_file = "G1A_Curso_01_inscripicones.csv"
                     
-                archivo_csv_path_2 = os.path.join(data_path_2, name_file)
+            archivo_csv_path_2 = os.path.join(data_path_2, name_file)
 
-                with open(archivo_csv_path_2, 'a', newline='') as archivo_csv_2:
-                        escritor_csv_2 = csv.writer(archivo_csv_2)
+            with open(archivo_csv_path_2, 'a', newline='') as archivo_csv_2:
+                escritor_csv_2 = csv.writer(archivo_csv_2)
+                
+                if archivo_csv_2.tell() == 0:
+                    encabezados_curso_01 = ["usuario", "Nombre", "Forma_pago"]
+                    escritor_csv_2.writerow(encabezados_curso_01)
+                            
+                    while True:
+                        ins_user = input("Ingresa tu usuario por favor: ")
+                        ins_nombre = input("Ingresa tu nombre: ")
+                        print("¿Cémo deseas pagar tu inscripción?")
+                        print("En línea con tarjeta de credito [pulsa 1]")
+                        print("En efectivo [pulsa 2]")
+
+                        f_pago = input("Ingresa tu forma de pago: ")
+                        escritor_csv_2.writerow([ins_user,ins_nombre, f_pago])
                         
-                        if archivo_csv_2.tell() == 0:
-                            encabezados_curso_01 = ["usuario", "Nombre", "Forma_pago"]
-                            escritor_csv_2.writerow(encabezados_curso_01)
-                            
-                        while True:
-                            ins_user = input("Ingresa tu usuario por favor: ")
-                            ins_nombre = input("Ingresa tu nombre: ")
-                            print("¿Cémo deseas pagar tu inscripción?")
-                            print("En línea con tarjeta de credito [pulsa 1]")
-                            print("En efectivo [pulsa 2]")
+                        print("Felicidades has apartado tu lugar")
+                        print("")
 
-                            f_pago = input("Ingresa tu forma de pago: ")
-                            escritor_csv_2.writerow([ins_user,ins_nombre, f_pago])
+                        print("Debes completar tu registro antes del 08 de marzo")
+                        print("")
 
-                            archivo_csv_2.close()
+                        print("Datos del curso \n Fecha: 10 de marzo \n Ubicación: Online")
 
-                            print("Felicidades has apartado tu lugar")
-                            print("")
+                        break
 
-                            print("Debes completar tu registro antes del 08 de marzo")
-                            print("")
-
-                            print("Datos del curso \n Fecha: 10 de marzo \n Ubicación: Online")
-
-                            break
+                    archivo_csv_2.close()
             
-            elif fecha_elegida == 1 and horario_elegido == "B":
-                name_file = "G1B_Curso_01_inscripicones.csv"
-                            
-                archivo_csv_path_2 = os.path.join(data_path_2, name_file)
-
-                with open(archivo_csv_path_2, 'a', newline='') as archivo_csv_2:
+                elif fecha_elegida == 1 and horario_elegido == "B":
+                     name_file = "G1B_Curso_01_inscripicones.csv"
+                     archivo_csv_path_2 = os.path.join(data_path_2, name_file)
+                     
+                     with open(archivo_csv_path_2, 'a', newline='') as archivo_csv_2:
                         escritor_csv_2 = csv.writer(archivo_csv_2)
 
                         if archivo_csv_2.tell() == 0:
@@ -160,8 +166,6 @@ if eleccion == "INSCRIBIRME":
                             f_pago = input("Ingresa tu forma de pago: ")
                             escritor_csv_2.writerow([ins_user,ins_nombre, f_pago])
 
-                            archivo_csv_2.close()
-
                             print("Felicidades has apartado tu lugar")
                             print("")
 
@@ -171,6 +175,8 @@ if eleccion == "INSCRIBIRME":
                             print("Datos del curso \n Fecha: 10 de marzo \n Ubicación: Online")
 
                             break
+                        
+                        archivo_csv_2.close()
 
             elif fecha_elegida == 1 and horario_elegido == "C":
                 name_file = "G1C_Curso_01_inscripicones.csv"
@@ -412,7 +418,8 @@ if eleccion == "INSCRIBIRME":
                             print("Datos del curso \n Fecha: 05 de abril \n Ubicación: Online")
 
                             break
-                                                
+
+                                                                   
 elif eleccion == "INSTRUCCIONES":
     print("Sigue los siguientes pasos para apartar tu lugar: ")
     print("")
