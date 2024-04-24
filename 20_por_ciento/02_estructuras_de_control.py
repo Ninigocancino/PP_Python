dfcf2ad4c50137b2d469e7ca854d37777dc00836
VictorCancino <juanvictorcancino@gmail.com>
2024-04-24 11:24:08 -0600

    Sé agregó el ejercició uno y se completó

---
+++ 20_por_ciento/02_estructuras_de_control.py
@@ -0,0 +1,10 @@
+#ESTRUCTURAS DE CONTROL CONDICIONAL
+
+#Ejercicio 01: Crea una instrucción que imprima si 'aceptado' si una persona tiene más de 18 años, de lo contrario deberá imprimir 'denegado'
+
+edad = 16
+
+if edad >= 18:
+    print("Aceptado")
+else:
+    print("Denegado")
