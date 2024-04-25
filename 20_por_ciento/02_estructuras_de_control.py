a8207d54adb844fb66c671588b7c79458929bf0f
VictorCancino <juanvictorcancino@gmail.com>
2024-04-25 09:33:41 -0600

    se completo el ejercicio 2 dentro del fichero '02_estructuras_de_control.py'

--- 20_por_ciento/02_estructuras_de_control.py
+++ 20_por_ciento/02_estructuras_de_control.py
@@ -2,9 +2,26 @@
 
 #Ejercicio 01: Crea una instrucción que imprima si 'aceptado' si una persona tiene más de 18 años, de lo contrario deberá imprimir 'denegado'
 
+print("EJERCICIO 'MAYOR DE EDAD' ")
+
 edad = 16
 
 if edad >= 18:
     print("Aceptado")
 else:
     print("Denegado")
+
+print("")
+
+#Ejercicio 02: Crea una instrucción que solicite el nombre al usuario y valide si es el propietario de un objeto. El programa debe imprimir feedback
+
+print("EJERCICIO '¿ESTO ES TUYO?' ")
+
+solicitar_nombre = input("Ingresa tu nombre por favor:").upper()
+
+duenio = "PERLA"
+
+if solicitar_nombre == duenio:
+    print("Este objeto es de tu propiedad")
+else:
+    print("Este objeto no te pertenece")
