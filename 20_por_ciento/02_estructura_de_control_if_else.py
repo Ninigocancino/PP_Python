aef0134de61a6a1e653c4625a771e69befd4e32a
VictorCancino <juanvictorcancino@gmail.com>
2024-04-25 10:14:40 -0600

    Se completo el ejercicio 3 en el fichero '02_estructura_de_control condicional_if_else.py'

--- 20_por_ciento/02_estructura_de_control_if_else.py
+++ 20_por_ciento/02_estructura_de_control_if_else.py
@@ -25,3 +25,22 @@ if solicitar_nombre == duenio:
     print("Este objeto es de tu propiedad")
 else:
     print("Este objeto no te pertenece")
+
+#Ejercicio 03: Crea un programa que pida al usuario la temperatura y devuelva un recomendación con base en el dato ingresado
+
+print("EJERCICIO 'TEMPERATURA' ")
+
+temp = float(input("Ingresa la temperatura actual(grados centigrados): "))
+
+if temp < 0:
+    print("Hace demasiado frio, no salgas a esta temperatura se congela el agua")
+elif temp >= 0 and temp <= 10:
+    print("Hace mucho frío, asegurate de abrigarte muy bien")
+elif temp >10 and temp <= 20:
+    print("Esta templado pudes salir, pero no olvides un abrigo ligero")
+elif temp >20 and temp <= 24:
+    print("El clima esta fresco puedes salir con ropa ligera")
+elif temp >24 and temp <= 30:
+    print("Es caluroso procura usar ropa comoda y tomar agua")
+else:
+    print("Es demasiado caluroso, no te expongas al sol y toma mucha agua")
