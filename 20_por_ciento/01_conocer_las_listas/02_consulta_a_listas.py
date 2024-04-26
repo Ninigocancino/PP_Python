0de04efe785540ef949fa4adcb8c0cf178eb93d8
VictorCancino <juanvictorcancino@gmail.com>
2024-04-26 09:14:14 -0600

    En el fichero '02_consulta_a_listas.py' se crearón tres listas que son los recursos para realizar los ejercicios de este apartado del proyecto

---
+++ 20_por_ciento/01_conocer_las_listas/02_consulta_a_listas.py
@@ -0,0 +1,7 @@
+#Listas a trabajar en el fichero:
+
+lista_ciudades = ['México','Lisboa','San Fracisco','Monterrey','Bogota','Madrid','Barcelona','París','Berlin','Pekin','Manila','Alejandría', 'Caracas','Toronto','Buenos Aires', 'Montevideo','Cancún','Ciudad del Cabo','Londres','Río de Janeiro','Tokio','Monaco','Playa del Carmen']
+
+lista_frutas = ['Guanabana','Manzana','Pera','Cereza','Fresa','Uva','Melon','Sandía','Coco','Mango','Pitahaya','Tuna','Tomate','Aguacate','Guayaba','Piña','Papaya','Zapote']
+
+lista_nombres = ['German','Juan','Francisco','Iker','Laura','Maria','Mary','Lucio','Cuahutemoc','Xochitl','Marina','Kate','Lucrecia', 'Malinalli','Saul', 'Levi', 'Domingo', 'Dominic', 'John', 'Joe', 'Luca']
