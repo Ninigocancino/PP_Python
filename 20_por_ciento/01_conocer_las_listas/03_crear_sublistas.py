d2116dab3f0f821beffda2eb4ce49bea2f5aef65
VictorCancino <juanvictorcancino@gmail.com>
2024-04-26 10:28:50 -0600

    En el fichero '03_crear_sublistas.py' del subdirectorio '01_conocer_las_listas' se agregarón las listas 'lista_ciudades' y 'lista_plantas' que son los recursos necesarios para resolver los ejercicio del fichero

---
+++ 20_por_ciento/01_conocer_las_listas/03_crear_sublistas.py
@@ -0,0 +1,5 @@
+#Recursos para resolver los ejercicios
+
+lista_ciudades = ['Monterrey','Villehermosa','Oaxaca','La Paz','Méxio','Cancún','Palenque','Veracruz','Madrid','Barcelona','Bilbao','Zaragoza','Merida','León','Ibiza','Tenerife','Londres','Manchester','Liverpol','Bogota','Cali','Medellín','Cartagena','Roma','Milan','Florencia','Venecia','Napoles','Pizza','Turino']
+
+lista_plantas = ['Cebolla','Zanahoria','Papa','Repollo','tomate','aguacate','fresa','uva','mango','platano','Pino','Cedro','Ceiba','caoba','Cecoya','Hule']
