813fdb84f15f8ad3b7cbda404e2f030f4a04f758
VictorCancino <juanvictorcancino@gmail.com>
2024-04-30 22:19:44 -0600

    Se completo el ejercicio 03 en el fichero '02_ciclo_for.py'

--- 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for.py
+++ 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for.py
@@ -6,7 +6,7 @@ for i in ciudades:
     print(i)
 
 
-# Ejercicio 02: Dadoun diccionario recorre cada uno de sus elementos e imprime por separado las clave y los valores del iterable
+# Ejercicio 02: Dado un diccionario recorre cada uno de sus elementos e imprime por separado las clave y los valores del iterable
 print("")
 lugar = {'Ciudad':'Ciudad de México', 'País' : 'México'}
 
@@ -14,3 +14,12 @@ for clave in lugar:
     print(clave)
 for valor in lugar.values():
     print(valor)
+
+#Ejercicio 03: Crea una lista con un solo elemento, luego recorre el elemento para imprimir en consola cada letra
+
+print("")
+
+palabra = ['Perro']
+
+for i in palabra[0]:
+    print(i)
