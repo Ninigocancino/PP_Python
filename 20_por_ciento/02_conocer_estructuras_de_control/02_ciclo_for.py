0a1376a5e4fb3fc7c872326b466d5d60ceebae34
VictorCancino <juanvictorcancino@gmail.com>
2024-04-30 22:05:34 -0600

    Se completo el ejercicio 2 en el fichero '02_ciclo_for.py'

--- 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for.py
+++ 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for.py
@@ -4,3 +4,13 @@ ciudades = ['Bogota','Cali','México','Paris']
 
 for i in ciudades:
     print(i)
+
+
+# Ejercicio 02: Dadoun diccionario recorre cada uno de sus elementos e imprime por separado las clave y los valores del iterable
+print("")
+lugar = {'Ciudad':'Ciudad de México', 'País' : 'México'}
+
+for clave in lugar:
+    print(clave)
+for valor in lugar.values():
+    print(valor)
