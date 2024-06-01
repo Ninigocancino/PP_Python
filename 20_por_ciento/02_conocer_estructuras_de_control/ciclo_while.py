5ce421b6181eb7c8c1e8a97acf3d938188c3df83
VictorCancino <juanvictorcancino@gmail.com>
2024-06-01 11:33:25 -0600

    Se completó el ejercicio 01 en la sección 'implementación básica del ciclo while' en el fichero 'ciclo_while.py'

---
+++ 20_por_ciento/02_conocer_estructuras_de_control/ciclo_while.py
@@ -0,0 +1,19 @@
+# IMPLEMENTACIÓN BÁSICA DEL CICLO WHILE
+
+#Ejercicio 01: Itera una lista numérica  mientras el número sea menor al resultado de una operación dada
+print("*"*20)
+print("EJERCICIO 01: CONTADOR")
+print("")
+
+a = 3
+b = 2
+operacion = a*b
+
+contador = 0
+
+while operacion > contador:
+    print(F"contador es: {contador}")
+    contador += 1
+
+print("")
+
