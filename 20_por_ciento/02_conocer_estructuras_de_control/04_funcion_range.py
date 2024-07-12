72cab9e0809b0e63d1c030ff0750dc45e120e6f6
VictorCancino <juanvictorcancino@gmail.com>
2024-07-11 23:58:50 -0600

    Se completó el ejercicio 02 del fichero '04_funcion_range'

--- 20_por_ciento/02_conocer_estructuras_de_control/04_funcion_range.py
+++ 20_por_ciento/02_conocer_estructuras_de_control/04_funcion_range.py
@@ -7,3 +7,14 @@ for i in range(7):
     print(i)
 
 print("")
+
+# Ejercicio 02: Crea un ciclo for y has que imprima una cuenta que inicia en el número 9 y termina en el numero 15
+
+print(""*15)
+print("EJERCICIO 02:")
+print("")
+
+for i in range(9, 15):
+    print(i)
+
+print("")
