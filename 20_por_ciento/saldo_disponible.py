01ad1dfeab2e0912af6e976f9ab66511ad67f4d9
VictorCancino <juanvictorcancino@gmail.com>
2024-07-23 22:15:22 -0600

    Se agregó el código para permiter la entrada de datos al programa

--- 20_por_ciento/saldo_disponible.py
+++ 20_por_ciento/saldo_disponible.py
@@ -4,3 +4,17 @@
         #Utiliza los conceptos que has aprendido en los directorios anteriores para resolver el problema siguiente.
 
         #PROBLEMA: Supón que quieres tener un mayor control de tus gastos para cuidar tu dinero, por lo que decides crear un programa que te ayude a cuidar tu dinero el programa te permite establecer un tope de gastos al enviarte un mensaje de advertencia cuando tus gastos han superado un monto establecido previamente por ti
+
+
+print(" ")
+print((("-")*10),("CONTROLA TUS GASTOS"),(("-")*10))
+print("Bienvenido a tu controlador de gastos, por favor sigue las instrucciones")
+print("")
+
+print(((" ")*10),"¿Listo para iniciar?")
+triger = int(input("ingresa (1) para continuar y (2) para salir: "))
+
+if triger == 1:
+    print("Ok")
+else:
+    exit()
