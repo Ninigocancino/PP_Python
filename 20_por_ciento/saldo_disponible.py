922105f0117d674d49181a8f0e37c27ed736ce43
VictorCancino <juanvictorcancino@gmail.com>
2024-07-23 22:00:03 -0600

    Se agregarón las instrucciones del proyecto 'saldo_disponible.py'

---
+++ 20_por_ciento/saldo_disponible.py
@@ -0,0 +1,6 @@
+#_________________________________PROYECTO: CONTADOR DE SALDO DISPONIBLE________________________________
+
+#-------INSTRUCCIONES:
+        #Utiliza los conceptos que has aprendido en los directorios anteriores para resolver el problema siguiente.
+
+        #PROBLEMA: Supón que quieres tener un mayor control de tus gastos para cuidar tu dinero, por lo que decides crear un programa que te ayude a cuidar tu dinero el programa te permite establecer un tope de gastos al enviarte un mensaje de advertencia cuando tus gastos han superado un monto establecido previamente por ti
