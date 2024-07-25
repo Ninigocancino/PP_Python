e9673b50582bb639bc058b2f8405974b5a66c84f
VictorCancino <juanvictorcancino@gmail.com>
2024-07-24 23:14:40 -0600

    Se agregó la funcionalidad de agregar puntos a un elemento record dentro del programa quiz.py

--- 20_por_ciento/quiz.py
+++ 20_por_ciento/quiz.py
@@ -1,4 +1,5 @@
 
+record = []
 respuesta_q1 = "B"
 
 print("¿Cuál es el país más grande del mundo?")
@@ -11,9 +12,20 @@ q1 = input("Ingresa tu respuesta: ").upper()
 
 if q1 == respuesta_q1:
     print("Perfecto has acertado")
+    print("Haz ganado 5 puntos")
+    pts_q1 = 5
+    record.append(pts_q1)
 elif q1 == "C":
     print("Bierut, no es un país")
+    pts_q1 = 0
+    record.append(pts_q1)
 elif q1 == "A":
     print("Muy cerca, China es grande pero no es el más grande")
+    pts_q1 = 0
+    record.append(pts_q1)
 else:
     print("Elige una opción valida")
+    pts_q1 = 0
+    record.append(pts_q1)
+
+print(f"Tu record es de {record} pts")
