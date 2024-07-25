879a03235fcd8823f592cf714368fbb67e48311f
VictorCancino <juanvictorcancino@gmail.com>
2024-07-24 23:01:16 -0600

    Se agregó al programa la estructura de control básica para evaluar las respuestas del usuario

---
+++ 20_por_ciento/quiz.py
@@ -0,0 +1,19 @@
+
+respuesta_q1 = "B"
+
+print("¿Cuál es el país más grande del mundo?")
+print("Opción A: China")
+print("Opción B: Rusia")
+print("Opción C: Beirut")
+print("")
+
+q1 = input("Ingresa tu respuesta: ").upper()
+
+if q1 == respuesta_q1:
+    print("Perfecto has acertado")
+elif q1 == "C":
+    print("Bierut, no es un país")
+elif q1 == "A":
+    print("Muy cerca, China es grande pero no es el más grande")
+else:
+    print("Elige una opción valida")
