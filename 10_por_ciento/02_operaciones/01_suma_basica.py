5216aaf4ca2d283ddfff30169b7e4cfa95e18291
VictorCancino <juanvictorcancino@gmail.com>
2024-08-03 13:04:24 -0600

    Se completo el ejercicio 'operación suma básica' en el fichero '01_suma_basica.py' y se documento el código y el ejercicio

---
+++ 10_por_ciento/02_operaciones/01_suma_basica.py
@@ -0,0 +1,13 @@
+"""
+Nombre del ejercicio: Operación suma básica
+
+Instrucciones: Crea dos variables (puede ser 'a' y 'b') y asignales un valor númerico cualquiera a cada una de ellas, después crea una tercera variable que guarde la suma entre las variables 'a' y 'b', por último imprime el resultado en consola y ejecuta el código para verificar si el programa funciona
+"""
+
+#Operación suma:
+a = 2
+b = 3
+
+suma = a + b
+
+print(suma)
