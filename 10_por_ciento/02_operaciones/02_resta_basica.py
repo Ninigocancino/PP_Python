8d845259f298e40b01f66089230479023e3183d8
VictorCancino <juanvictorcancino@gmail.com>
2024-08-03 13:35:38 -0600

    Se creó el archivo '02_resta_basica.py', se completó el ejercicio 'operación resta'

---
+++ 10_por_ciento/02_operaciones/02_resta_basica.py
@@ -0,0 +1,14 @@
+"""
+Nombre del ejercicio: Operación suma básica
+
+Instrucciones: Crea dos variables y asignales un valor númerico cualquiera a cada una de ellas, después crea una tercera variable que guarde la resta entre las variables creadas, por último imprime el resultado en consola y ejecuta el código para verificar si el programa funciona
+"""
+#Se crean las variables que guradarán los valores a usar en la operación 
+r_a = 8
+r_b = 10
+
+#Se crea la variable que guardará la operación suma
+resta = r_b - r_a
+
+#Impresión del resultado
+print(resta)
