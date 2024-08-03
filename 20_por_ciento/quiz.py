978a194b183d2f3a62a8cbf06e6a14efaf77fc1c
VictorCancino <juanvictorcancino@gmail.com>
2024-08-03 12:37:50 -0600

    Se documentó y se corrigierón errores no significativos en el proyecto 'quiz'

--- 20_por_ciento/quiz.py
+++ 20_por_ciento/quiz.py
@@ -1,16 +1,35 @@
+"""
+Nombre del ejercicio: Quiz básico
 
+Instrucciones:
+Crea un quiz de dos preguntas con opciones múltiples y utiliza una estructura de control para indicar al programa que hacer cuándo la respuesta es correcta, cuando es incorrecta o cuando no es una opción valida 
+
+Criterios de calidad:
+1.- El programa debe permitir la interacción con el usuario 
+2.- El programa debe incorparar al menos un manejo de errores
+3.- El programa debe permitir que el usuario sume puntos por cada respuesta correcta. La solución del código uede ser elemental o poco sofisticada.
+4.- El programa debe mostrar al usuario los puntos acumulados
+"""
+
+
+#Guarda una lista vacia que recibirá las respuestas ingresadas por el usuario
 record = []
+
+#Guardan las respuestas correctas a cada una de las preguntas
 respuesta_q1 = "B"
 respuesta_q2 = "C"
 
+#Se la información relacionada con la pregunta número 1 del quiz
 print("¿Cuál es el país más grande del mundo?")
 print("Opción A: China")
 print("Opción B: Rusia")
 print("Opción C: Beirut")
 print("")
 
-q1 = input("Ingresa tu respuesta: ").upper()
+#Permite al usuario ingresar datos para interactuar con el programa
+q1 = input("Ingresa tu respuesta: ").upper() 
 
+#Con las estructuras if,elif,else evaluaremos si el usuario a ingresado una respuesta valida 
 if q1 == respuesta_q1:
     print("Perfecto has acertado")
     print("Haz ganado 1 punto")
@@ -23,27 +42,36 @@ elif q1 == "A":
 else:
     print("Elige una opción valida")
 
-
+#Se imprime la información relacionada con la pregunta número 2 del quiz
 print("¿Cuál es el país que limita al sur con los E.U.A.?")
 print("Opción A: China")
 print("Opción B: Rusia")
 print("Opción C: México")
 print("")
 
+#Permite al usuario ingresar datos para interactuar con el programa
 q2 = input("Ingresa tu respuesta: ").upper()
 
+
+#Con las estructuras if,elif,else evaluaremos si el usuario a ingresado una respuesta valida y se agrega la letra de la opción a la lista record de ser correcta
 if q2 == respuesta_q2:
     print("Perfecto has acertado")
     print("Haz ganado 1 punto")
-    pts_q1 = "C"
-    record.append(pts_q1)
-elif q1 == "A":
+    pts_q2 = "C"
+    record.append(pts_q2)
+elif q2 == "A":
     print("China, no se encuentra al sur de E.U.A.")
-elif q1 == "B":
+elif q2 == "B":
     print("Rusia está cerca de E.U.A, pero no al sur")
 else:
     print("Elige una opción valida")
 
+#Se cuentan los elementos contenidos en la lista
 puntos = len(record)
 
+#Se imprime la retroalimentación para el usuario
 print(f"Tu record es de {puntos} pts")
+
+"""
+En esta solución se ha optado por abordar la lógica del programa de forma sencilla en la que cada vez que el usuario responde de forma correcta una de las preguntas del quiz el rograma 'toma' la opción correcta y la agrega a la lista vacia 'record', después el programa cuenta los elementos, en este enfoque cada respuesta correcta aporta 1 punto, al final el conteo de elementos en la lista es equivalente al numero de respuestas correctas donde cada una aporta un punto y final muestra un mensaje de retroalimentación para el usuario donde se incluye el puntaje obtenido.  
+"""
