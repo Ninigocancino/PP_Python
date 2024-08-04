01aa07da2bbf83b2f0e75ed30ac65355c0969f33
VictorCancino <juanvictorcancino@gmail.com>
2024-08-03 23:59:46 -0600

    Se completó el ejercicio 'soy una variable' en el fichero '02_mensaje_desde_variable.py' y se documento el código

---
+++ 10_por_ciento/01_mostrar_en_consola/02_mensaje_desde_variable.py
@@ -0,0 +1,10 @@
+"""
+Nombre del ejercicio: Soy una variable
+
+Instrucciones: Crea una variable que guarde el valor string 'Soy una variable' y después usa la función 'print' para imprimir en consola el mensaje
+"""
+
+#Declaración de la variable
+mensaje = "Hola Mundo 2"
+
+print(mensaje)
