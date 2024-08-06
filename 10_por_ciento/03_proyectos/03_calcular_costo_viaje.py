6e56aa2ac796a9c03e7153526da2807ef0a5a59c
VictorCancino <juanvictorcancino@gmail.com>
2024-08-06 07:59:46 -0600

    Se modificó el planteamiento del proyecto 'Calculadora de gastos de viaje' en el fichero '03_calculadora_costo_viaje.py'

--- 10_por_ciento/03_proyectos/03_calcular_costo_viaje.py
+++ 10_por_ciento/03_proyectos/03_calcular_costo_viaje.py
@@ -1,21 +1,26 @@
 """
-Supongamos que debes realizar un viaje en automovil de una ciudad 'A' a una ciudad 'B', laciudad 'A' se encuentra a 80 km de distancia 
-en línea recta, sin embargo ninguno de los dos caminos que conectan ambas ciudades siguen una línea recta perfecta.
-En cambio el camino 1 cuenta con muchas curvas y en total se deben recorrer 130 km para llegar a la ciudad 'B',
+NOMBRE DEL PROYECTO:  Calculadora de gastos de viaje 
+
+DESCRICPÓN DEL PROYECTO: 
+Supongamos que debes realizar un viaje en automovil de una ciudad 'A' a una ciudad 'B', la ciudad 'A' se encuentra a 80 km de distancia en línea recta, sin embargo ninguno de los dos caminos que conectan ambas ciudades siguen una línea recta perfecta.
+
+En cambio, el camino 1 cuenta con muchas curvas y en total se deben recorrer 130 km para llegar a la ciudad 'B',
 por su parte el camino 2 es más directo y solo deben recorrerse 100 km para llegar a la ciudad 'B' pero para tomar
 este camino debe pagarse un peaje de $250 para vehiculos pequeños, $350 para vehiculos medianos y $700 para vehiculos pesados.
 
 Como cuentas con presupuesto limitado para este viaje necesitas cuidar al máximo el dinero y economizar.
 
-Haz deicidido viajar en un vehiculo pequeño que tiene un consumo de 10 litros por kilometro en caminos con pocas curvas
+Haz decidido viajar en un vehículo pequeño que tiene un consumo de 10 litros por kilometro en caminos con pocas curvas
 y 18 litros por kilometro en caminos pronunicados o con muchas curvas.
 
 El precio de la gasolina a la fecha de tu salida es de $25 por litro.
 
-"""
+INSTRUCCIONES:
+Crea un programa sencillo que realice las siguientes acciones:
+  1.- Calcula el gasto total de cada opción para elegir el más económico
+  2.- Muestra el desarrollo del problema al usuario usando print
 
-#Calcula el gasto total de cada opción para elegir el más económico
-#Muestra eld esarrollo del problema al usuario usando print
+"""
 
 
 print("Datos analisis 1: ")
