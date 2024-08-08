f73fd50a596841af08f752e6482bb34e3c4a1551
VictorCancino <juanvictorcancino@gmail.com>
2024-08-08 10:16:12 -0600

    Se agregarón las intsruciones del proyecto 'Quiz V_01'

---
+++ 10_por_ciento/03_proyectos/01_quiz_v1.py
@@ -0,0 +1,13 @@
+"""
+Nombre del royecto: Quiz V_01
+
+Instrucciones:
+Crearemos un programa de preguntas y respuestas pero antes de pasar a la parte de la logíca de programación queremos que el usuario conozco de que tratará nuestro programa por lo que debemos imprimir en la pantallas la decripción del programa y las instrucciones básicas. 
+
+Criterios de calidad:
+1.- El programa debe imprimir un mensaje de bienvenida
+2.- El programa debe debe imprimir una descripción del programa
+3.- El programa debe debe imprimir al menos dos de las instrucciones del juego
+4.- Los textos deben mostrarse de forma amigable para facilitar al usuario su lectura 
+"""
+
