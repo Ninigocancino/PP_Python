8f5de502ecfc65734fc1e87b250a9b5bb6d6dbed
VictorCancino <juanvictorcancino@gmail.com>
2024-08-08 12:09:14 -0600

    Se agregó la estructura de descricpon de proyecto al proyecto ''Inventario Total' en el fichero '04_administrador_de_inventario_v1.py'

---
+++ 10_por_ciento/03_proyectos/04_administrador_de_inventario_v1.py
@@ -0,0 +1,20 @@
+"""
+INFORMACIÓN DEL PROYECTO:
+
+Nombre del Proyecto: Inventario Total
+
+Descripción:
+Crearemos un programa que permita controlar el inventario de un grupo comercial cuya principal línea de negocios son los supermercados y tiendas de conveniencia. Esta empresa distribuye los diferentes articulos de su stock desde su centro logístico a los almacenes insitu de cada sucursal de las distintas marcas que forman parte del grupo. El grupo comercial esta creciendo rápidamente y quiere digitalizar cada parte del negocio, pues aún no cuenta cuenta con una infraestructura digital que le permita ser competitivo con empresas de mayor tamaño. El gerente de logistica te ha contratado para que crear un programa que le ayude a controlar mejor el inventario y que pueda hacer más eficiente toda la operación del departamente y facilitar la entrega de información a otros departamentos relacionados. 
+
+Instrucciones:
+Se requiere que el proyecto se aborde por bloques de funcionalidades para poder detectar errores e incorporar mejoras de forma más ágil y esbelta.
+
+Caso de uso:
+Se quiere desarrollar el proyecto partiendo de la interacción con una de las tiendas más pequeñas y de menor volumen de ventas para establecer un entorno controlado, para evitar un consumo innecesario de recursos para este proyecto. Por lo que el primer paso es notificar al gerente de la tienda las ruebas que se estrán realizando.
+
+Historia de usuario:
+
+
+Criterios de calidad:
+1.- 
+"""
