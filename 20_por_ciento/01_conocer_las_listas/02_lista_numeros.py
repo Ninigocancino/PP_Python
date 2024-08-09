55b712e6717460617d40f714ebb607fae20e13ca
VictorCancino <juanvictorcancino@gmail.com>
2024-08-09 09:04:15 -0600

    Se agregó y competó el ejercicio 'Lista de números' al fichero '02_lista_numeros.py'

---
+++ 20_por_ciento/01_conocer_las_listas/02_lista_numeros.py
@@ -0,0 +1,10 @@
+"""
+Nombre del ejercicio: Lista de números
+
+Instrucciones: Crea una lista con numeros del 0 al 20 e imprime el contenido.
+"""
+
+print("")
+print("Lista 2:")
+numeros= [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+print(numeros)
