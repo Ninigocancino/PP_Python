0fcfff1c61ddd4b8e8f2eed3b3b4d205b00efb22
VictorCancino <juanvictorcancino@gmail.com>
2024-08-09 09:11:13 -0600

    Se agregó el ejercicio 'Lista de animales domésticos' al fichero '03_lista-animales.py'

---
+++ 20_por_ciento/01_conocer_las_listas/03_lista_animales.py
@@ -0,0 +1,10 @@
+"""
+Nombre del ejercicio: Lista de nombres
+
+Instrucciones: Crea una lista de 5 elementos con nombres de animales domesticos e imprime el contenido.
+"""
+
+print("")
+print("Lista animales domésticos:")
+animales= ['Perro','Gato','Hamster','Perico','Caballo']
+print(animales)
