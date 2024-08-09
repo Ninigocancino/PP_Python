f6a883a5a7182bf96156806b2e96b98175e1b9c7
VictorCancino <juanvictorcancino@gmail.com>
2024-08-09 08:56:54 -0600

    Se agregó y completó el ejercicio 'Lista de nombres' al fichero '01_lista_nombres.py'

---
+++ 20_por_ciento/01_conocer_las_listas/01_lista_nombres.py
@@ -0,0 +1,9 @@
+"""
+Nombre del ejercicio: Lista de nombres
+
+Instrucciones: Crea una lista con 5 elementos, los elementos deben ser nombres de personas, después imprime el contenido de la lista en la consola
+"""
+
+print("Lista Nombres:")
+nombres= ['Juan','Pablo','Lisa','Sebastian','Laura']
+print(nombres)
