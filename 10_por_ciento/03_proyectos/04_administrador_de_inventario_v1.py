b9056768f7be1304f77d0317571eb3eee9eeb52b
VictorCancino <juanvictorcancino@gmail.com>
2024-08-09 07:38:00 -0600

    Se completó la sección 'Información del proyecto' en el fichero '04-administrador_de_inventario_v1.py'

--- 10_por_ciento/03_proyectos/04_administrador_de_inventario_v1.py
+++ 10_por_ciento/03_proyectos/04_administrador_de_inventario_v1.py
@@ -13,8 +13,16 @@ Caso de uso:
 Se quiere desarrollar el proyecto partiendo de la interacción con una de las tiendas más pequeñas y de menor volumen de ventas para establecer un entorno controlado, para evitar un consumo innecesario de recursos para este proyecto. Por lo que el primer paso es notificar al gerente de la tienda las ruebas que se estrán realizando.
 
 Historia de usuario:
+Cómo gerente de sucursal necesito tener información general del programa que me permita saber que acciones debo realizar para poder usarlo o cuál es la razón para hacerlo
 
 
 Criterios de calidad:
-1.- 
+Crear un programa sencillo que realice las siguientes acciones: 
+1.- El programa debe imprimir un mensaje de bienvenida
+2.- El programa debe debe imprimir una descripción del programa
+3.- El programa debe debe imprimir al menos dos de las instrucciones del programa
+4.- Los textos deben mostrarse de forma amigable para facilitar al usuario su lectura 
+5.- El programa debe mostrar al usuario un ejemplo sencillo de la utilidad que tendrá el programa, mostrando la reducción del stock con una compra.
+6.- El programa debe mostrar la información de la transacción de forma desglosada
+7.- El programa debe mostrar un mensaje de despedida antes de cerrarse
 """
