1416d3ac914ed74719ac2b05d09a8e043f2c840e
VictorCancino <juanvictorcancino@gmail.com>
2024-08-11 13:36:31 -0600

    Se agregarón las instrucciones del ejercicio

---
+++ 20_por_ciento/02_conocer_estructuras_de_control/if_else/03_clima_de_hoy.py
@@ -0,0 +1,8 @@
+"""
+Nombre del ejercicio: ¿Esto es tuyo?
+
+Instrucciones: 
+1.- Crea un programa que pida al usuario la temperatura y devuelva un recomendación con base en el dato ingresado
+
+
+"""
