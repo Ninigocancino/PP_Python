2f13420e123b4b6912aac5b7f29445bc5afcad4b
VictorCancino <juanvictorcancino@gmail.com>
2024-08-11 13:28:01 -0600

    Se agregó una instrucción faltante al ejercicio

--- 20_por_ciento/02_conocer_estructuras_de_control/if_else/02_propietario.py
+++ 20_por_ciento/02_conocer_estructuras_de_control/if_else/02_propietario.py
@@ -4,7 +4,8 @@ Nombre del ejercicio: ¿Esto es tuyo?
 Instrucciones: 
 1.- Crea un script que pregunte el nombre del usuario para validar si un objeto le pertenece o no
 2.- Si el objeto le pertenece se debe imprimir el mensaje 'aquí lo tiene, disculpe el inconveniente'
-3.- De no pertenecer debe imprimir el mensaje 'Lo sentimos su nombre no coincide con el del propietario' 
+3.- De no pertenecer debe imprimir el mensaje 'Lo sentimos su nombre no coincide con el del propietario'
+4.- El programa debe prevenir que el input no coincida con el valor  'correcto' por uso minúscula o mayúsculas 
 
 """
 
