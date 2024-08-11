07aa6c2658e55749e649e74b58c92e3b96aa0126
VictorCancino <juanvictorcancino@gmail.com>
2024-08-10 23:16:19 -0600

    Se completó el ejercicio 'Crear sublista de plantas'

--- 20_por_ciento/01_conocer_las_listas/02_lista_plantas.py
+++ 20_por_ciento/01_conocer_las_listas/02_lista_plantas.py
@@ -8,3 +8,21 @@ Instrucciones:
 3.- Crea una sublista de plantas que son arboles.
 4.- Crea una sublista de plantas que son vegetales.
 """
+
+plantas = ["Roble", "Pino", "Eucalipto", "Nogal", "Manzano", "Pera", "Cerezo", "Naranjo", "Limonero", "Olivo", "Castaño", "Haya", "Arce", "Abedul", "Cedro","Manzana", "Pera", "Cereza", "Naranja", "Limón", "Banana", "Uva", "Fresa", "Frambuesa", "Mora", "Melón", "Sandía", "Piña", "Mango", "Kiwi","Lechuga", "Tomate", "Pepino", "Zanahoria", "Cebolla", "Pimiento", "Brócoli", "Coliflor", "Espinaca", "Ajo", "Patata", "Calabacín","Rosa", "Lavanda", "Jazmín", "Azalea", "Camelia", "Rododendro", "Hibiscus", "Boj"]
+
+
+print("")
+elementos_lista = len(plantas)
+print(f"La lista tiene {elementos_lista} elementos")
+print("")
+
+
+arboles = plantas[0:14]
+print(arboles)
+print("")
+
+
+vegetales = plantas[32:41]
+print(vegetales)
+print("")
