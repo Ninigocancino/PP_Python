462954373a71d5ccc5dd066755a01df2c0f2ae0c
VictorCancino <juanvictorcancino@gmail.com>
2024-08-11 13:38:41 -0600

    Se modificó el nombre del ejercicio

--- 20_por_ciento/02_conocer_estructuras_de_control/if_else/03_clima_de_hoy.py
+++ 20_por_ciento/02_conocer_estructuras_de_control/if_else/03_clima_de_hoy.py
@@ -1,5 +1,5 @@
 """
-Nombre del ejercicio: ¿Esto es tuyo?
+Nombre del ejercicio: ¿Cómo está el clima hoy?
 
 Instrucciones: 
 1.- Crea un programa que pida al usuario la temperatura y devuelva un recomendación con base en el dato ingresado
