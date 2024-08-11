3dcb5619ec7a3a241cf2f656cc7a3f2c21c89a1a
VictorCancino <juanvictorcancino@gmail.com>
2024-08-10 22:19:03 -0600

    Se agregarón las instrucciones del ejercicio 'Crear sublistas de ciudades de paises'

---
+++ 20_por_ciento/01_conocer_las_listas/01_lista_ciudades.py
@@ -0,0 +1,10 @@
+"""
+Nombre del ejercicio: Crear sub lista de ciudades de paises 
+
+Instrucciones: 
+1.- Crea una lista, usa el comando 'Alt + z' para que el editor te muestre el contenido con salto de línea y escribe tres línea con nombres de ciudades 'agrupandolas' por el país al que pertenecen.
+2.- Imprime el contenido de la lista.
+3.- Identifica el indice de la última ciudad de cada país.
+3.- Crea una sublista de ciudades mexicana.
+4.- Crea una sublista de ciudades italianas. 
+"""
