3d8ab9eb67308f3e3d60728ae5a40e9e8e0a0992
VictorCancino <juanvictorcancino@gmail.com>
2024-08-10 23:00:51 -0600

    Se agregarón las instrucciones del ejercicio 'Crear siblistas de plantas'

---
+++ 20_por_ciento/01_conocer_las_listas/02_lista_plantas.py
@@ -0,0 +1,10 @@
+"""
+Nombre del ejercicio: Crear sub lista de plantas
+
+Instrucciones: 
+1.- Crea una lista, usa el comando 'Alt + z' para que el editor te muestre el contenido con salto de línea y escribe tres línea con nombres de plantas 'agrupandolos' de acuerdo a su tipo.
+2.- Imprime el número de elementos de la lista.
+3.- Identifica el indice de la última última planta de cada tipo.
+3.- Crea una sublista de plantas que son arboles.
+4.- Crea una sublista de plantas que son vegetales.
+"""
