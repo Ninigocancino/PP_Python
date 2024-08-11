1d8f9930a8dd8c95333666cebe753a3597ccb74e
VictorCancino <juanvictorcancino@gmail.com>
2024-08-11 13:13:32 -0600

    Se agregarón las instrucciones del ejercicio '¿Esto es tuyo?'

---
+++ 20_por_ciento/02_conocer_estructuras_de_control/if_else/02_propietario.py
@@ -0,0 +1,9 @@
+"""
+Nombre del ejercicio: ¿Esto es tuyo?
+
+Instrucciones: 
+1.- Crea un script que pregunte el nombre del usuario para validar si un objeto le pertenece o no
+2.- Si el objeto le pertenece se debe imprimir el mensaje 'aquí lo tiene, disculpe el inconveniente'
+3.- De no pertenecer debe imprimir el mensaje 'Lo sentimos su nombre no coincide con el del propietario' 
+
+"""
