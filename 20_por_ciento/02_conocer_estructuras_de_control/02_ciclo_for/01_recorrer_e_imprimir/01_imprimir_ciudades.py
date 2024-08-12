eaa804af28496681668b94db17ea92715a2f4b50
VictorCancino <juanvictorcancino@gmail.com>
2024-08-12 10:41:53 -0600

    Se agregarón las instrucciones del ejercicio 'Imprimir las ciudades'

---
+++ 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for/01_recorrer_e_imprimir/01_imprimir_ciudades.py
@@ -0,0 +1,9 @@
+"""
+Nombre del ejercicio: Imprimir las ciudades 
+
+Instrucciones: 
+1.- Crea una lista de ciudades
+2.- Recorre cada una de las ciudades contenidas en la lista
+3.- Imprime cada uno de los elementos de la lista
+
+"""
