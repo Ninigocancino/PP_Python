45138081379e616b368b6c14c6f43d293d7249c0
VictorCancino <juanvictorcancino@gmail.com>
2024-08-12 11:07:27 -0600

    Se agregarón las instrucciones del ejercicio 'Imprime los paises'

---
+++ 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for/01_recorrer_e_imprimir/03_imprime_los_paises.py
@@ -0,0 +1,9 @@
+"""
+Nombre del ejercicio: Imprime los paises
+
+Instrucciones: 
+1.- Crea una lista de nombres de paise
+2.- Usa un ciclo for para recorrer cada elemento de la lista 
+3.- Imprime cada uno de los elementos de la lista
+
+"""
