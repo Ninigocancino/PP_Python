235dc7a8b891f7c4f1a90ed041acdfc10bb032bb
VictorCancino <juanvictorcancino@gmail.com>
2024-08-12 11:23:57 -0600

    Se agregarón las instrucciones  del ejercicio 'Imprime el lugar'

---
+++ 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for/01_recorrer_e_imprimir/04_imprimir _el_lugar.py
@@ -0,0 +1,9 @@
+"""
+Nombre del ejercicio: Imprime los paises
+
+Instrucciones: 
+1.- Crea un diccionario de donde la claves sean  'País' y 'ciudad' respectivamente.
+2.- Usa un ciclo for para recorrer cada elemento del diccionario.
+3.- Imprime los valores del diccionario para indicar el par ciudad país
+
+"""
