d3e8ac40f5632648e6f0cf4db2123befd4783db3
VictorCancino <juanvictorcancino@gmail.com>
2024-08-15 11:38:54 -0600

    Se agregarón las instrucciones del ejercicio 'Palabra'

---
+++ 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for/02_recorrer_sobre_un_elemento/01_palabra.py
@@ -0,0 +1,9 @@
+"""
+Nombre del ejercicio: Palabra
+
+Instrucciones: 
+1.- Crea una lista con un solo elemento.
+2.- Usa un ciclo for para recorrer cada letra del elemento en la lista.
+3.- Imprime los valores de la lista.
+
+"""
