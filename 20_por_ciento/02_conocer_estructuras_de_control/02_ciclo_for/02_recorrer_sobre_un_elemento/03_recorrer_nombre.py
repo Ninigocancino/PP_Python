7aa00cda7dae1ca27e75c2a8d912e74a1338df47
VictorCancino <juanvictorcancino@gmail.com>
2024-08-15 12:05:57 -0600

    Se completó el ejercicio '03_recorrer_nombre.py'

--- 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for/02_recorrer_sobre_un_elemento/03_recorrer_nombre.py
+++ 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for/02_recorrer_sobre_un_elemento/03_recorrer_nombre.py
@@ -3,7 +3,12 @@ Nombre del ejercicio: Recorre un nombre
 
 Instrucciones: 
 1.- Crea una lista de nombres de personas
-2.- Usa un ciclo for para recorrer un elemento de la lista 
+2.- Usa un ciclo for para recorrer el elemento de la lista que se encuentra en el indice 2
 3.- Imprime cada uno de los elementos de la lista
 
 """
+
+nombres = ['Julio','Marco', 'Zacarias','Sandra', 'Felipe','María', 'Anahi']
+
+for i in nombres[2]:
+    print(i)
