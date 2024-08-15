2b451fb01c8521f7a2a5ce3d297eb86ab3a7ba88
VictorCancino <juanvictorcancino@gmail.com>
2024-08-15 11:31:10 -0600

    Se agregarón las instrucciones del ejercicio 'Imprime el nombre los animales'

---
+++ 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for/01_recorrer_e_imprimir/05_imprimir_n_animales.py
@@ -0,0 +1,9 @@
+"""
+Nombre del ejercicio: Imprime el nombre de los animales
+
+Instrucciones: 
+1.- Crea una lista con nombres de animales.
+2.- Usa un ciclo for para recorrer cada elemento del diccionario.
+3.- Imprime los valores de la lista.
+
+"""
