4d35437860c63c4983dd933bb683d3951cccae61
VictorCancino <juanvictorcancino@gmail.com>
2024-08-15 11:45:27 -0600

    Se creó el fichero 'variable' y se agregarón las instrucciones del ejercicio 'Recorre la variable'

---
+++ 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for/02_recorrer_sobre_un_elemento/02_variable.py
@@ -0,0 +1,9 @@
+"""
+Nombre del ejercicio: Recorre la variable
+
+Instrucciones: 
+1.- Crea una una varibale con un valor cualquiera.
+2.- Usa un ciclo for para recorrer el elemnto que guarda la variable.
+3.- Imprime el resultado.
+
+"""
