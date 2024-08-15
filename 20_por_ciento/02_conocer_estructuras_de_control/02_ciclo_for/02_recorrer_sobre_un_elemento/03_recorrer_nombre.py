38be92b036fc3291743c4daa5e7ed98f732d7bbc
VictorCancino <juanvictorcancino@gmail.com>
2024-08-15 12:02:42 -0600

    Se agregarón las intsrucciones del ejercicio 'Recorrer un nombre'

---
+++ 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for/02_recorrer_sobre_un_elemento/03_recorrer_nombre.py
@@ -0,0 +1,9 @@
+"""
+Nombre del ejercicio: Recorre un nombre
+
+Instrucciones: 
+1.- Crea una lista de nombres de personas
+2.- Usa un ciclo for para recorrer un elemento de la lista 
+3.- Imprime cada uno de los elementos de la lista
+
+"""
