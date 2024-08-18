652bdebec221dce7326735e8202b00f8b7d2da32
VictorCancino <juanvictorcancino@gmail.com>
2024-08-17 21:37:27 -0600

    Se agregarón las instrucciones del ejercicio 'Rango numérico'

---
+++ 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for/03_bucles_y_rangos/01_rango_simple.py
@@ -0,0 +1,9 @@
+"""
+Nombre del ejercicio: Rango numérico 
+
+Instrucciones: 
+1.- Crea un rango que vaya de 0 a 5
+2.- Usa un ciclo for para recorrer cada uno de los números en el rango
+3.- Imprime cada uno de los elementos de la lista
+
+"""
