bf72ce51016f9bdf52e24aa7ccc24e03404b9b40
VictorCancino <juanvictorcancino@gmail.com>
2024-08-17 22:36:55 -0600

    Se agregarón las instrucciones al ejercicio 'Encuentra el país'

---
+++ 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for/04_usando_if/01_pais_en_lista.py
@@ -0,0 +1,12 @@
+"""
+Nombre del ejercicio: Encuentra el país
+
+Instrucciones: 
+1.- Crea una lista de paises 
+2.- Usa un ciclo for para recorrer los elementos 
+3.- Busca el valor 'México' en la lista
+4.- Si el valor existe imprime una confirmación de existencia
+5.- Si no existe imprime una negación
+
+"""
+
