8596c98fb45327fcb85bb762520e1d7a20b248f6
VictorCancino <juanvictorcancino@gmail.com>
2024-08-17 21:43:11 -0600

    Se agregarón las instrucciones del ejercicio 'Rango con incrementos'

---
+++ 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for/03_bucles_y_rangos/02_rangos_con_incrementos.py
@@ -0,0 +1,9 @@
+"""
+Nombre del ejercicio: Rango con incrementos
+
+Instrucciones: 
+1.- Crea un rango que vaya de 0 a 10 pero con saltos de 10 en 10
+2.- Usa un ciclo for para recorrer los incrementos en el rango
+3.- Imprime cada uno de los incrementos
+
+"""
