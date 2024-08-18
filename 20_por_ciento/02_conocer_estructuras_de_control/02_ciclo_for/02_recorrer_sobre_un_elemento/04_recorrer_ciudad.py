1001af740cb0687d455b066509a98f27937f88f3
VictorCancino <juanvictorcancino@gmail.com>
2024-08-17 21:25:04 -0600

    Se agregarón las instrucciones del ejercicio 'Recorrer el nombre de una ciudad'

---
+++ 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for/02_recorrer_sobre_un_elemento/04_recorrer_ciudad.py
@@ -0,0 +1,9 @@
+"""
+Nombre del ejercicio: Recorre un el nombre de una ciudad
+
+Instrucciones: 
+1.- Crea una lista con nombres de ciudades
+2.- Usa un ciclo for para recorrer el elemento de la lista que se encuentra en el indice 5
+3.- Imprime cada uno de los elementos de la lista
+
+"""
