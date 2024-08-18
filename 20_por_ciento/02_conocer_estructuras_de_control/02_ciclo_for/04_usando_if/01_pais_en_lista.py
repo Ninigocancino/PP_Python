0db66ea269056b3d5564c9f2e1e9ceac9d8edb1f
VictorCancino <juanvictorcancino@gmail.com>
2024-08-17 22:55:44 -0600

    Se completó el ejercicio 'Encuentra el país'

--- 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for/04_usando_if/01_pais_en_lista.py
+++ 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for/04_usando_if/01_pais_en_lista.py
@@ -3,10 +3,18 @@ Nombre del ejercicio: Encuentra el país
 
 Instrucciones: 
 1.- Crea una lista de paises 
-2.- Usa un ciclo for para recorrer los elementos 
-3.- Busca el valor 'México' en la lista
-4.- Si el valor existe imprime una confirmación de existencia
-5.- Si no existe imprime una negación
+2.- Usa un ciclo for para recorrer los elementos y buscar el valor 'México' en la lista
+3.- Si el valor existe imprime una confirmación de existencia
+4.- Si no existe imprime una negación
 
 """
+
+paises = ['México','Sudafrica','Canada','Colombia','Cuba']
+
+for i in paises:
+    if i == "México":
+        print(f"Aquí esta {i}")
+    else:
+        print(f"Este país es {i}")
+
 
