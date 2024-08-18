e3756a0c41443bc1ae7b5c788c0e36710d652313
VictorCancino <juanvictorcancino@gmail.com>
2024-08-17 21:49:48 -0600

    Se agregarón las instrucciones del ejercicio 'Incrementos elevados al cuadrado'

---
+++ 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for/03_bucles_y_rangos/03_rango_operacion.py
@@ -0,0 +1,10 @@
+"""
+Nombre del ejercicio: Incrementos elevados al cuadrado
+
+Instrucciones: 
+1.- Crea un rango que vaya de 0 a 100 pero con saltos de 5 en 5
+2.- Usa un ciclo for para recorrer los incrementos en el rango
+3.- Eleva los incrementos al cuadrado
+4.- Imprime cada uno de los incrementos
+
+"""
