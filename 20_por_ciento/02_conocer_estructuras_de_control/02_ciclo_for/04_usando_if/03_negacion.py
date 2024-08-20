6d454774aeb6e40e8863e3e2bb5c11cf7aa3b095
VictorCancino <juanvictorcancino@gmail.com>
2024-08-19 19:23:08 -0600

    Se agregarón las instrucciones del ejercicio 'Animal en lista'

---
+++ 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for/04_usando_if/03_negacion.py
@@ -0,0 +1,9 @@
+"""
+Nombre del ejercicio: Animal en lista
+
+Instrucciones: 
+1.- Crea una lista de animales y niega que 'Canguro' se encuentre en la lista
+2.- Usa un ciclo for para recorrer cada elemento
+3.- Imprime '____ está en la lista' para cada animal que no hayas negado y para canguro 'este animal no esta en la lista'
+
+"""
