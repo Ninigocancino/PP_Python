68b636112b0370d33802b5ad94d69c5af6071f45
VictorCancino <juanvictorcancino@gmail.com>
2024-08-19 19:08:28 -0600

    Se agregarón las instrucciones del ejercicio 'Imprimir los números pares en un rango dado'

---
+++ 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for/04_usando_if/02_imprimir_pares.py
@@ -0,0 +1,9 @@
+"""
+Nombre del ejercicio: Imprimir los números pares enun rango dado
+
+Instrucciones: 
+1.- Crea un rango del 0 al 20
+2.- Usa un ciclo for para recorrer el rango mientras los números sean pares
+3.- Imprime los valores 
+
+"""
