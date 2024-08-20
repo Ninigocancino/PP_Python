e34e71c2e92a3b618c10b7ffe9dbdd2fb486f1d1
VictorCancino <juanvictorcancino@gmail.com>
2024-08-19 19:39:16 -0600

    Se completó el ejercicio

--- 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for/04_usando_if/03_negacion.py
+++ 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for/04_usando_if/03_negacion.py
@@ -7,3 +7,22 @@ Instrucciones:
 3.- Imprime '____ está en la lista' para cada animal que no hayas negado y para canguro 'este animal no esta en la lista'
 
 """
+
+animales = ['Lobo', 'Zorro', 'Hipopotamo', 'Comadreja','León', 'Jaguar']
+
+print("")
+print("Vamos si tenemos un Canguro")
+print("")
+animal_buscado = input("Ingresa la palabara Canguro: ")
+
+
+for animal in animales:
+    if not animal == animal_buscado:
+        print("")
+        print(f"encontre un {animal} en la lista")
+        if not animal_buscado in animales:
+            print(f"pero no encontre un {animal_buscado} en la lista")
+            print("")
+    else:
+        print(f"No pude buscar este animal {animal_buscado}")
+        print("Pero...")
