b30b7daad507ff9886d9f4096e88b37c654d047f
VictorCancino <juanvictorcancino@gmail.com>
2024-08-23 21:41:39 -0600

    Se agregarón las instrucciones del ejercicio 'Tablas de multiplicar del 1 al 10'

---
+++ 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for/05_ciclos_anidados/02_tabla_multiplicar.py
@@ -0,0 +1,8 @@
+"""
+Nombre del ejercicio: Tablas de multiplicar del 1 al 10
+
+Instrucciones: 
+1.- Usa el ciclo for para crear lastablas de multiplicar del 1 al 10
+2.- Imprime cada una de las tablas
+
+"""
