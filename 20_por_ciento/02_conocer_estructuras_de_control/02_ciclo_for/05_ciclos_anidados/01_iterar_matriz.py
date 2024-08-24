51f3434ea7681b206b6ccb50a401d5de59adaaa4
VictorCancino <juanvictorcancino@gmail.com>
2024-08-23 21:30:02 -0600

    Se agregarón las instrucciones del ejercicio 'Iterar elementos de una matriz númerica'

---
+++ 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for/05_ciclos_anidados/01_iterar_matriz.py
@@ -0,0 +1,9 @@
+"""
+Nombre del ejercicio: Iterar elementos de una matriz
+
+Instrucciones: 
+1.- Crea una matriz de elementos numericos desde una lista de lista 
+2.- Usa un ciclo for para recorrer cada elemento
+3.- Imprime cada fila de la matriz
+
+"""
