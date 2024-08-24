7d0b42835174e68e464c2c18c1acfc87a056c9b9
VictorCancino <juanvictorcancino@gmail.com>
2024-08-23 22:57:19 -0600

    Se trajó la versión 01 del proyecto y se adaptarón las instrucciones  y requerimientos

---
+++ 20_por_ciento/03_proyectos/01_quiz_v2.py
@@ -0,0 +1,34 @@
+"""
+Nombre del proyecto: Quiz V_02
+
+Instrucciones:
+
+Ahora que hemos mostrado al usuario lo que puede hacer con nuestro programa añadiremos una nueva capa de complejidad a nuestro Quiz, por lo que agregaremos a nuestro programa nuestras primeras dos preguntas con opciones múltiples, deberemos utilizar estructura de control para indicar al programa que hacer cuándo la respuesta es correcta, cuando es incorrecta o cuando no es una opción valida 
+
+Criterios de calidad:
+1.- El programa debe permitir la interacción con el usuario 
+2.- El programa debe incorparar al menos un manejo de errores
+3.- El programa debe permitir que el usuario sume puntos por cada respuesta correcta. La solución del código puede ser elemental o poco sofisticada.
+4.- El programa debe mostrar al usuario los puntos acumulados
+
+"""
+
+print("")
+
+print("_"*60)
+
+print("")
+print("*"*20,"QUIZ V_01","*"*20)
+print(" "*16,"Bienvenido, juguemos")
+
+print("")
+
+print("-"*18, "Instrucciones","-"*18)
+print("")
+print(" "*4,"01 Ingresa iniciar")
+print(" "*4,"02 Responde las reguntas para ganar puntos")
+print("")
+
+print("_"*60)
+
+print("")
