1250087fc1ca1436eba3fa7d645222c2e53de9e8
VictorCancino <juanvictorcancino@gmail.com>
2024-08-23 22:17:56 -0600

    Se agregarón las instrucciones del ejercicio 'Iterar sobre lista de diccionario'

---
+++ 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for/05_ciclos_anidados/03_lista_diccionario.py
@@ -0,0 +1,10 @@
+"""
+Nombre del ejercicio: Itera sobre lista de diccionario
+
+Instrucciones: 
+1.- Crea un diccionario con dos elementos que contienen listas
+2.- Itera sobre cada elemento
+2.- Imprime el resultado
+
+"""
+
