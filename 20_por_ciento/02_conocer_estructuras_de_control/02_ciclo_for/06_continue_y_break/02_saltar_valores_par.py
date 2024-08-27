6b42055dfe86759233da98c0fb1579c4587d479e
VictorCancino <juanvictorcancino@gmail.com>
2024-08-26 21:43:34 -0600

    Se agregarón las instrucciones del ejercicio en el fichero '02_saltar_valores_par.py'

---
+++ 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for/06_continue_y_break/02_saltar_valores_par.py
@@ -0,0 +1,9 @@
+"""
+Nombre del ejercicio: Saltar los valores par de una secuencia numérica
+
+Instrucciones: 
+1.- Crea una secuencia del 1 al 20
+2.- Has que el programa identifique los números que sean par y los salte
+3.- Imprime el resto de los números
+
+"""
