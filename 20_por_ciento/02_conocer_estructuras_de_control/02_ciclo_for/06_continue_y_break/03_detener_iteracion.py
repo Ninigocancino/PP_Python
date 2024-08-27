8020cda642ab2920a50d2d89fde9118bea928840
VictorCancino <juanvictorcancino@gmail.com>
2024-08-26 21:50:02 -0600

    Se agregarón las instrucciones del ejercicio en el fichero '03_detener_iteracion.py'

---
+++ 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for/06_continue_y_break/03_detener_iteracion.py
@@ -0,0 +1,9 @@
+"""
+Nombre del ejercicio: Detener la iteración de una lista
+
+Instrucciones: 
+1.- Crea una lista con almenos 5 elementos
+2.- Has que el programa se detenga cuando encuentre un valores especifico en la lista
+3.- Imprime los elementos antes del break
+
+"""
