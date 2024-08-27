c8604155b4e36592767422560ddef9f8232c38ce
VictorCancino <juanvictorcancino@gmail.com>
2024-08-26 21:32:29 -0600

    Se agregarón las instrucciónes del proyecto en el fichero '02_administrador_de_ofertas_v2.py'

---
+++ 20_por_ciento/03_proyectos/02_administrador_de_ofertas_v2.py
@@ -0,0 +1,25 @@
+"""
+DESCRIPCIÓN DEL PROYECTO:
+
+Nombre del Proyecto: Descuentapp
+
+Instrucciones:
+Crearemos un programa que permita a una tienda administrar sus ofertas y conocer la información relacionada a cada una de ellas y al mismo tiempo sea capaz de mostrar a los clientes el desglose de información referente a su compra.
+
+Se requiere que el proyecto se aborde por bloques de funcionalidades para poder detectar errores e incorporar mejoras de forma más ágil y esbelta.
+
+Caso de uso:
+En una sucursal de la tienda algunos productos cuentan con un 7% de descuentos mientras otros no cuentan con descuento 
+
+Historia de usuario:
+Como comprador me gustaría poder saber que productos cuentan con descuento y cuales no cuentan con descuento, además de conocer cuanto es el mónto que ahorrare con respecto del precio normal
+
+Criterios de calidad:
+Además de los requisitos de la versión anterior...
+1.- El programa debe permitir la interacción del usuario con el programa
+2.- El programa debe mostrar si un producto ingresado por el usuario tiene descuento 
+3.- El programa debe calcular el total de la compra
+4.- El programa debe mostrar al usuario el ahorro en su compra
+6.- El programa debe mostrar la información de la transacción de forma desglosada
+7.- El programa debe mostrar un mensaje de despedida antes de cerrarse
+"""
