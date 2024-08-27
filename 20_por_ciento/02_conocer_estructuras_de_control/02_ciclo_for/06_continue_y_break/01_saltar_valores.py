d29f4fa02580c9f4dc6840d6a8daf82b6b8fc0d3
VictorCancino <juanvictorcancino@gmail.com>
2024-08-26 21:38:41 -0600

    Se agregarón las instrucciones del ejercicio en el fichero '01_saltar_valores.py'

---
+++ 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for/06_continue_y_break/01_saltar_valores.py
@@ -0,0 +1,8 @@
+"""
+Nombre del ejercicio: Saltar valores de una secuencia numérica
+
+Instrucciones: 
+1.- Salta dos valores de una secuencia del 1 al 20
+2.- Imprime el resto de los números
+
+"""
