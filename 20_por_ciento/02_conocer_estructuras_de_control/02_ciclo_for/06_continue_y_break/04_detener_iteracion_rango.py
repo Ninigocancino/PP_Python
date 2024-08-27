c408ee1b284992ebe7e9a30f42f8a82e1339be43
VictorCancino <juanvictorcancino@gmail.com>
2024-08-26 22:00:17 -0600

    Se agregarón las instrucciones del ejercicio en el fichero '04_detener_iteracion_rango.py'

---
+++ 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for/06_continue_y_break/04_detener_iteracion_rango.py
@@ -0,0 +1,9 @@
+"""
+Nombre del ejercicio: Detener la iteración de un rango
+
+Instrucciones: 
+1.- Crea un rango del 0 al 20
+2.- Has que el programa se detenga cuando el valor del iterable sea igual a la multiplicación de 2 por 8 
+3.- Imprime los elementos antes del break
+
+"""
