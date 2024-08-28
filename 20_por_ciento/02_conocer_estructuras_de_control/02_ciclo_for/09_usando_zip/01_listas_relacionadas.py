1fd2d9604e0fb7ae1a41e585802312b958a164ce
VictorCancino <juanvictorcancino@gmail.com>
2024-08-28 12:31:11 -0600

    Se agregarón las instrucciones del ejercicio

---
+++ 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for/09_usando_zip/01_listas_relacionadas.py
@@ -0,0 +1,9 @@
+"""
+Nombre del ejercicio: Emparejar dos listas
+
+Instrucciones: 
+1.- Crea dos listas relacionadas conceptualmente
+2.- Usa el método ZIP para iterarlas simultaneamente. Las listas deben contener el mismo número de elementos (al menos tres por cada listas)
+3.- Imprime un mensaje que contenga las iteraciones simultaneas
+
+"""
