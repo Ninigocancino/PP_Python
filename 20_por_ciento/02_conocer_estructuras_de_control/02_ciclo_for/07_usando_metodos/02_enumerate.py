bfedf42849a9196bc894a3388286e15be834ba13
VictorCancino <juanvictorcancino@gmail.com>
2024-08-27 22:31:57 -0600

    Se agregarón las instrucciones del ejercicio

---
+++ 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for/07_usando_metodos/02_enumerate.py
@@ -0,0 +1,9 @@
+"""
+Nombre del ejercicio: Descubre el indice del país
+
+Instrucciones: 
+1.- Crea una lista con nombres de paises
+2.- Recorre cada elemento de la lista
+3.- Imprime los elementos y sus indices en la lista
+
+"""
