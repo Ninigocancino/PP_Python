b2edf61b5dbba1bc866bbbd008c2e644a4eb91f4
VictorCancino <juanvictorcancino@gmail.com>
2024-08-28 12:50:04 -0600

    Se completó el ejercicio 'Emparejar dos listas de diferente tamaño'

--- 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for/09_usando_zip/03_listas_diferentes_tamaño.py
+++ 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for/09_usando_zip/03_listas_diferentes_tamaño.py
@@ -7,3 +7,10 @@ Instrucciones:
 3.- Imprime un mensaje que contenga las iteraciones simultaneas
 
 """
+
+titulos = ['Originales','Hyperfocus','Esencialismo','Repensar la pobreza']
+anios = [2014,2017,2019]
+
+for titulo,anio in zip(titulos,anios):
+    print(titulo,anio)
+
