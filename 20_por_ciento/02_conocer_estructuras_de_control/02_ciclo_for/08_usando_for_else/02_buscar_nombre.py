874c1254d1dbb315f67b9cabdd5147ec60fa41b0
VictorCancino <juanvictorcancino@gmail.com>
2024-08-28 10:33:27 -0600

    Se agregarón las instrucciones del ejercicio

---
+++ 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for/08_usando_for_else/02_buscar_nombre.py
@@ -0,0 +1,13 @@
+"""
+Nombre del ejercicio: Buscar un nombre en una lista
+
+Instrucciones: 
+1.- Crea una lista de nombres
+2.- Recorre los elementos de la lista
+3.- Guarda un nombre en una variable
+4:- Comprueba si el nombre esta en la lista
+5.- Si el nombre esta en la lista imprime 'el nombre esta en la lista'
+6.- Si el nombre no esta en la lista imprime 'el nombre no esta en la lista'
+
+"""
+
