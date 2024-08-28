01a2d47111f654e113c677f1f7f0dbefea82e649
VictorCancino <juanvictorcancino@gmail.com>
2024-08-28 12:37:32 -0600

    Se agregarón las instrucciones del ejercicio

---
+++ 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for/09_usando_zip/02_tuplas_relacionadas.py
@@ -0,0 +1,9 @@
+"""
+Nombre del ejercicio: Emparejar dos tuplas
+
+Instrucciones: 
+1.- Crea dos tuplas relacionadas conceptualmente
+2.- Usa el método ZIP para iterarlas simultaneamente. Las tuplas deben contener el mismo número de elementos (al menos tres por cada tupla)
+3.- Imprime un mensaje que contenga las iteraciones simultaneas
+
+"""
