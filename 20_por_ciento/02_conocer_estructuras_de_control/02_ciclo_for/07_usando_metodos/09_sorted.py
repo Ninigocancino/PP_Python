32050e170d1ec8d9e2cf6131657d2b5223668823
VictorCancino <juanvictorcancino@gmail.com>
2024-08-28 08:23:49 -0600

    Se agregarón las instrucciones del ejercicio

---
+++ 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for/07_usando_metodos/09_sorted.py
@@ -0,0 +1,10 @@
+"""
+Nombre del ejercicio: Ordena los nombres de una lista
+
+Instrucciones: 
+1.- Crea una lista con nombres
+2.- Recorre los elementos de la lista
+3.- Usa el método sorted para ordenar los elementos de la A a la Z
+3.- Imprime los nombres
+
+"""
