f490bfde9dee901f06d48da49c04c05355e48579
VictorCancino <juanvictorcancino@gmail.com>
2024-08-27 22:51:27 -0600

    Se completó el ejercicio 'Ficha de producto'

--- 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for/07_usando_metodos/04_items.py
+++ 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for/07_usando_metodos/04_items.py
@@ -7,3 +7,13 @@ Instrucciones:
 3.- Imprime la clave y el valor de cada elemnto
 
 """
+
+producto = {
+    "Nombre" : "Laptop",
+    "Precio" : "12550",
+    "Disponibilidad" : True,
+    "Caracteristicas" : ["i7", "16GB RAM", "512GB SSD"]
+}
+
+for c, v in producto.items():
+    print(f"{c}:{v}")
