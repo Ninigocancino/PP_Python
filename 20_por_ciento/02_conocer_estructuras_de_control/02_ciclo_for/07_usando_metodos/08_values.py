175b7da20449a0a0d86f7a6e509a73dff0f7199f
VictorCancino <juanvictorcancino@gmail.com>
2024-08-28 08:18:10 -0600

    Se agregarón las instrucciones del ejercicio

---
+++ 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for/07_usando_metodos/08_values.py
@@ -0,0 +1,10 @@
+"""
+Nombre del ejercicio: Traer los valores de un diccionario
+
+Instrucciones: 
+1.- Crea un diccionario cualquiera
+2.- Recorre los elementos del diccionario
+3.- Usa el método values para traer los valores del diccionario
+3.- Imprime los valores
+
+"""
