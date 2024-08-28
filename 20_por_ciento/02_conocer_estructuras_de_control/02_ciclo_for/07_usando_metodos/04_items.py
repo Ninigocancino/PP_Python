ac66cd26075e72e239f19093d5b7d6ac0a583323
VictorCancino <juanvictorcancino@gmail.com>
2024-08-27 22:50:09 -0600

    Se agregarón las instrucciones del ejercicio

---
+++ 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for/07_usando_metodos/04_items.py
@@ -0,0 +1,9 @@
+"""
+Nombre del ejercicio: Ficha de producto
+
+Instrucciones: 
+1.- Crea un diccionario que guarde información de un producto
+2.- Recorre cada elemento del diccionario
+3.- Imprime la clave y el valor de cada elemnto
+
+"""
