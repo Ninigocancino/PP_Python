86054c257ac2927b01717f45acf5008d022e607a
VictorCancino <juanvictorcancino@gmail.com>
2024-08-27 22:23:22 -0600

    Se agregarón las instrucciones del ejercicio

---
+++ 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for/07_usando_metodos/01_upper.py
@@ -0,0 +1,9 @@
+"""
+Nombre del ejercicio: Ciudades en mayúsculas
+
+Instrucciones: 
+1.- Crea una lista con nombres de ciudades en minúsculas
+2.- Recorre cada elemnto de la lista y conviertelos a mayúsculas
+3.- Imprime los elementos
+
+"""
