ae797e7711890192c410eb82af046e6003307b54
VictorCancino <juanvictorcancino@gmail.com>
2024-08-28 09:29:57 -0600

    Se agregarón las instrucciones del ejercicio

---
+++ 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for/08_usando_for_else/01_buscador.py
@@ -0,0 +1,10 @@
+"""
+Nombre del ejercicio: Buscar número en una lista
+
+Instrucciones: 
+1.- Crea una lista de números
+2.- Recorre los elementos de la lista
+3.- Si el número se encunetra en la lista imprime 'esta en la lista'
+4:- Si no esta en la lista impirme 'No esta en la lista
+
+"""
