d4c1cdf92d69c2729df1f92d526dfded59e90cca
VictorCancino <juanvictorcancino@gmail.com>
2024-08-28 12:46:48 -0600

    Se agregarón las instrucciones del ejercicio

---
+++ 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for/09_usando_zip/03_listas_diferentes_tamaño.py
@@ -0,0 +1,9 @@
+"""
+Nombre del ejercicio: Emparejar dos listas diferentes tamaños
+
+Instrucciones: 
+1.- Crea dos listas relacionadas conceptualmente
+2.- Usa el método ZIP para iterarlas simultaneamente. Las listas no deben contener el mismo número de elementos 
+3.- Imprime un mensaje que contenga las iteraciones simultaneas
+
+"""
