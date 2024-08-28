7e1af87ea63b8d0149a3acf469b7a5f36e817c21
VictorCancino <juanvictorcancino@gmail.com>
2024-08-28 07:44:22 -0600

    Se agregarón las instrucciones del ejercicio

---
+++ 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for/07_usando_metodos/06_actualizar_items.py
@@ -0,0 +1,10 @@
+"""
+Nombre del ejercicio: Actualizar precio de productos
+
+Instrucciones: 
+1.- Crea un diccionario con productos y su precio
+2.- Recorre los elementos del diccionario
+3.- Usa el método items para hacer que el precio de los productos se actualice a razon deun incremento de 2 veces su valor actual
+3.- Imprime los productos con sus recios actualizados
+
+"""
