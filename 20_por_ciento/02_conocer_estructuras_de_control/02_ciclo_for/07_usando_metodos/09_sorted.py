153b6a1002713fb84cd45d1eef64b79a1a4e468f
VictorCancino <juanvictorcancino@gmail.com>
2024-08-28 08:33:07 -0600

    Se corrigierón las instrucciones del ejercicio 'Ordena los nombres de una lista'

--- 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for/07_usando_metodos/09_sorted.py
+++ 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for/07_usando_metodos/09_sorted.py
@@ -5,7 +5,10 @@ Instrucciones:
 1.- Crea una lista con nombres
 2.- Recorre los elementos de la lista
 3.- Usa el método sorted para ordenar los elementos de la A a la Z
-3.- Imprime los nombres
+4.- Usa de nuevo el metódo sorted para ordenar los elementos de la Z a la A
+5.- Imprime el contenido de la lista
+6:- Imprime los elementos nombres de la A a la Z de forma vertical
+7:- Imprime los elementos nombres de la Z a la A de forma vertical
 
 """
 
