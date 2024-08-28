a1c1eee411403bba8b17ff0d9876c67140ab1b8a
VictorCancino <juanvictorcancino@gmail.com>
2024-08-28 10:53:27 -0600

    Se completó el ejercicio 'Verificar si la lista esta en orden de menor a mayor'

---
+++ 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for/08_usando_for_else/04_verificar_orden.py
@@ -0,0 +1,11 @@
+"""
+Nombre del ejercicio: Verificar si la lista esta en orden de menor a mayor
+
+Instrucciones: 
+1.- Crea una lista de números
+2.- Recorre los elementos de la lista
+3:- Comprueba si el número anterior en cada iteración es menor que el siguiente
+5.- Si los numeros cumplen la condición imprime 'Estan ordenados'
+6.- Si los números no están ordenados imprime 'No están ordenados' 
+
+"""
