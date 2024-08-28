ffd673e3598b1d9a1e51b05238df36e41bfa64e9
VictorCancino <juanvictorcancino@gmail.com>
2024-08-28 08:48:36 -0600

    Se agregarón las instrucciones del ejercicio

---
+++ 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for/07_usando_metodos/11_set.py
@@ -0,0 +1,10 @@
+"""
+Nombre del ejercicio: Elimina elementos duplicados
+
+Instrucciones: 
+1.- Crea una lista de números
+2.- Recorre los elementos de la lista
+3.- Usa el método set para eliminar de la lista los elementos repetidos
+7:- Imprime los elementos números de forma vertical
+
+"""
