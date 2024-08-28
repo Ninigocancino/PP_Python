9de8ab99c7c855e0b83f46214c32792d09ef46c9
VictorCancino <juanvictorcancino@gmail.com>
2024-08-28 08:42:53 -0600

    Se ajustarón las instrucciones del ejercicio

--- 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for/07_usando_metodos/10_reversed.py
+++ 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for/07_usando_metodos/10_reversed.py
@@ -4,7 +4,7 @@ Nombre del ejercicio: Ordena una lista numérica en modo invertido
 Instrucciones: 
 1.- Crea una lista de números
 2.- Recorre los elementos de la lista
-3.- Usa el método reversed para ordenar la lsita de mayor a menor
+3.- Usa el método reversed para ordenar la lsita del último elemnto al primero
 7:- Imprime los elementos números de forma vertical
 
 """
