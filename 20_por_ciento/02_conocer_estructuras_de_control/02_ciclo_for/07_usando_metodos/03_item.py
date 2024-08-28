0082faaa2ae8b4671ad6ec3a8869be01a1a92fe1
VictorCancino <juanvictorcancino@gmail.com>
2024-08-27 22:39:55 -0600

    Se agregarón las instrucciones del ejercicio

---
+++ 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for/07_usando_metodos/03_item.py
@@ -0,0 +1,9 @@
+"""
+Nombre del ejercicio: Traer clave y valor
+
+Instrucciones: 
+1.- Crea un diccionario 
+2.- Recorre cada elemento del diccionario
+3.- Imprime la clave y el valor de cada elemnto
+
+"""
