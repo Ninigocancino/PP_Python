8617aaaba607ef152bf82a08eef05b66bcecc95e
VictorCancino <juanvictorcancino@gmail.com>
2024-08-28 09:32:09 -0600

    Se complet´el ejrcicio 'Buscar número en una lista'

--- 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for/08_usando_for_else/01_buscador.py
+++ 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for/08_usando_for_else/01_buscador.py
@@ -8,3 +8,13 @@ Instrucciones:
 4:- Si no esta en la lista impirme 'No esta en la lista
 
 """
+
+l_numeros = [20,25,60,15, 10]
+buscar = 12
+
+for i in l_numeros:
+    if i == buscar:
+        print(f"El numero {buscar} se encuentra en la lista")
+        break
+else:
+    print(f"El numero {buscar} no se encuentra en la lista")
