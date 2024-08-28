c574976a2e3e31a89b27e529dd9a4243ebfa3718
VictorCancino <juanvictorcancino@gmail.com>
2024-08-28 10:40:33 -0600

    Se agregarón las instrucciones del ejercicio

---
+++ 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for/08_usando_for_else/03_verificar_valor_ingresado.py
@@ -0,0 +1,12 @@
+"""
+Nombre del ejercicio: Verificar si un valor ingresado coincide con los valores de una lista
+
+Instrucciones: 
+1.- Crea una lista de animales
+2.- Pida al usuario que ingrese un animal 
+3.- Recorre los elementos de la lista
+4:- Comprueba si el animal buscado por el usuario esta en la lista
+5.- Si el nombre esta en la lista imprime 'el nombre esta en la lista'
+6.- Si el nombre no esta en la lista imprime 'el nombre no esta en la lista'
+
+"""
