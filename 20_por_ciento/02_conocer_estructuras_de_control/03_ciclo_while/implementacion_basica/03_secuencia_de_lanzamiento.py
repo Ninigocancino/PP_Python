5c16cefd5f614bab6f21107af28e0a40b1f39d39
VictorCancino <juanvictorcancino@gmail.com>
2024-08-29 09:58:14 -0600

    Se agregarón las instrucciones del ejercicio 'Secuencia de lanzamiento personalizada'

---
+++ 20_por_ciento/02_conocer_estructuras_de_control/03_ciclo_while/implementacion_basica/03_secuencia_de_lanzamiento.py
@@ -0,0 +1,9 @@
+"""
+Nombre del ejercicio: Secuencia de lanzamiento personalizada
+
+Instrucciones: 
+1.- Pide al usuario que ingrese una cantidad deseada
+2.- Usa el ciclo while para decrecer la cantidad ingresada por el usuario hasta que se cumpla una condición especifica
+2.- Imprime el conteo 
+
+"""
