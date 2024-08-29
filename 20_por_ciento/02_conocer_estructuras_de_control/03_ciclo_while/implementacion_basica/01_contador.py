0315fe5d90e322fff16f098b6cb1ce01d9891422
VictorCancino <juanvictorcancino@gmail.com>
2024-08-29 09:45:04 -0600

    Se completó el ejercicio 'Contador'

--- 20_por_ciento/02_conocer_estructuras_de_control/03_ciclo_while/implementacion_basica/01_contador.py
+++ 20_por_ciento/02_conocer_estructuras_de_control/03_ciclo_while/implementacion_basica/01_contador.py
@@ -6,3 +6,13 @@ Instrucciones:
 2.- Imprime el conteo 
 
 """
+
+a = 20
+b = 2
+operacion = a*b
+
+contador = 0
+
+while operacion > contador:
+    print(F"contador es: {contador}")
+    contador += 1
