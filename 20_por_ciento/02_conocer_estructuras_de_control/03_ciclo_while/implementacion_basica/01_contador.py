c42f7a1a296198392d866dff184dba56a32459a5
VictorCancino <juanvictorcancino@gmail.com>
2024-08-29 09:41:29 -0600

    Se agregarón las instrucciones del ejercicio 'Contador'

---
+++ 20_por_ciento/02_conocer_estructuras_de_control/03_ciclo_while/implementacion_basica/01_contador.py
@@ -0,0 +1,8 @@
+"""
+Nombre del ejercicio: Contador
+
+Instrucciones: 
+1.- Usa el ciclo while para iterar números hasta que el iterador sea menor que una operación dada
+2.- Imprime el conteo 
+
+"""
