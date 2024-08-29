720a122d7e4dbdf29f1ce139e9315dd465bfb7ec
VictorCancino <juanvictorcancino@gmail.com>
2024-08-29 10:37:55 -0600

    Se completó el ejercicio 'Adivina la edad'

--- 20_por_ciento/02_conocer_estructuras_de_control/03_ciclo_while/02_while_true/02_adivina_la_edad.py
+++ 20_por_ciento/02_conocer_estructuras_de_control/03_ciclo_while/02_while_true/02_adivina_la_edad.py
@@ -8,3 +8,18 @@ Instrucciones:
 4.- Si las cifras coinciden imprime 'Acertaste, la edad es correcta'
 
 """
+
+print("Adivina que edad tiene Lucas")
+
+while True: 
+    edad_usuario = int(input("¿Qué edad crees que tiene Lucas?: "))
+    edad = 25
+    if edad_usuario < edad:
+        print("Uy! Lucas es un poco mayor")
+    elif edad_usuario > edad:
+        print("Lucas es un poco más jóven")
+    elif edad_usuario == edad:
+        break
+    #print("la edad no es correcta")
+
+print("Acertaste: la edad es correcta")
