f23995f624a213479e0db2dbd27fac3c9a845b62
VictorCancino <juanvictorcancino@gmail.com>
2024-08-29 09:50:02 -0600

    Se agregarón las instrucciones del ejercicio 'Cuenta regresiva'

---
+++ 20_por_ciento/02_conocer_estructuras_de_control/03_ciclo_while/implementacion_basica/02_cuenta_regresiva.py
@@ -0,0 +1,8 @@
+"""
+Nombre del ejercicio: Contador
+
+Instrucciones: 
+1.- Usa el ciclo while para decrecer una cuenta a partir de un número dado y hasta que se cumpla una condición especifica
+2.- Imprime el conteo 
+
+"""
