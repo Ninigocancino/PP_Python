3f1d4b4098a6b9706146d78a5be498b5dc86c37f
VictorCancino <juanvictorcancino@gmail.com>
2024-08-29 10:14:17 -0600

    Se agregarón las instrucciones del ejercicio 'Instrucción para salir de un programa'

---
+++ 20_por_ciento/02_conocer_estructuras_de_control/03_ciclo_while/02_while_true/01_instrucion_de_salida.py
@@ -0,0 +1,9 @@
+"""
+Nombre del ejercicio: Instrucción para salir de un programa
+
+Instrucciones: 
+1.- Usa while true para crear un programa que detenga su ejecución al recibir la instrucción 'exit'
+2.- Si la instrrucción no es la correcta imprime el mensaje 'La instrucción no es correcta'
+3.- Si la instrucción es correcta imprime el mensaje ' El programa se ejecutó correctamente' 
+
+"""
