deb897d66c90feb570e654b79c4115ec039581e5
VictorCancino <juanvictorcancino@gmail.com>
2024-08-29 10:30:02 -0600

    Se agregarón las instrucciones del ejercicio 'Adivina la edad'

---
+++ 20_por_ciento/02_conocer_estructuras_de_control/03_ciclo_while/02_while_true/02_adivina_la_edad.py
@@ -0,0 +1,10 @@
+"""
+Nombre del ejercicio: Adivina la edad 
+
+Instrucciones: 
+1.- Usa while true para crear un programa que le pida al usuario adivinar la edad de Lucas 
+2.- Compara la edad de Lucas con el número ingresado por el usuario
+3.- Si las cifras no coinciden imprime 'La edad no es correcta'
+4.- Si las cifras coinciden imprime 'Acertaste, la edad es correcta'
+
+"""
