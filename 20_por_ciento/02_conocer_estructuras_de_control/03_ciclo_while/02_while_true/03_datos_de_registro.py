9ddbc1a3d63757827f58c9be7ca5ba9425770fc2
VictorCancino <juanvictorcancino@gmail.com>
2024-08-30 22:20:07 -0600

    Se agregarón las instrucciones del ejercicio 'Validación de datos de registro'

---
+++ 20_por_ciento/02_conocer_estructuras_de_control/03_ciclo_while/02_while_true/03_datos_de_registro.py
@@ -0,0 +1,11 @@
+"""
+Nombre del ejercicio: Validació de datos de registro
+
+Instrucciones: 
+1.- Crea datos de control
+2.- Pregunta al usuario su nombre, edad y altura
+3.- Usa While True para comparar si los datos ingresados concuerdadn con los de control
+4.- Si los datos coinciden imprime 'Hola ___ ¿qué quieres hacer?
+5.- Si los datos no coinciden imprime 'No eres ___'
+
+"""
