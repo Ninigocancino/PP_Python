2ba08ee7c0b859dd56e97a2040ddd1d2d1f42428
VictorCancino <juanvictorcancino@gmail.com>
2024-08-30 22:24:25 -0600

    Se completó el ejercicio 'Validación de datos de registro'

--- 20_por_ciento/02_conocer_estructuras_de_control/03_ciclo_while/02_while_true/03_datos_de_registro.py
+++ 20_por_ciento/02_conocer_estructuras_de_control/03_ciclo_while/02_while_true/03_datos_de_registro.py
@@ -9,3 +9,18 @@ Instrucciones:
 5.- Si los datos no coinciden imprime 'No eres ___'
 
 """
+
+while True:
+    edad_usuario = 45
+    nombre_usuario = "Lucas"
+    altura_usuario = 1.70
+
+    edad_g = int(input("Ingresa tu edad: "))
+    nombre_u = input("Ingresa tu nombre: ")
+    altura_u = float(input("Ingresa tu estatura:"))
+
+    if edad_usuario == edad_g and nombre_usuario == nombre_u and altura_usuario == altura_u:
+        break
+    print("No eres Lucas, intenta de nuevo")
+
+print("Hola Lucas ¿Qué quieres hacer?")
