03230a250bd3fb89660239d147cc99037a9b272f
VictorCancino <juanvictorcancino@gmail.com>
2024-08-30 22:30:15 -0600

    Se agregarón las instrucciones del ejercicio 'Conteo simultaneo'

---
+++ 20_por_ciento/02_conocer_estructuras_de_control/03_ciclo_while/03_while_anidado/01_conteo_simultaneo.py
@@ -0,0 +1,8 @@
+"""
+Nombre del ejercicio: Conteo simultaneo
+
+Instrucciones: 
+1.- Crea un conteo que controle la ejecución de otro conteo mientras la cuenta en ambos conteos sea menor de tres 
+2.- Imprime las combinaciones posibles
+
+"""
