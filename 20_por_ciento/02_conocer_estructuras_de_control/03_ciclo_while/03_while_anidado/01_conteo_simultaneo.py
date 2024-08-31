da59aeac702fe49eeac1927cd373a1c47cd7e902
VictorCancino <juanvictorcancino@gmail.com>
2024-08-30 22:32:03 -0600

    Se completó el ejercicio 'Conteo simultaneo'

--- 20_por_ciento/02_conocer_estructuras_de_control/03_ciclo_while/03_while_anidado/01_conteo_simultaneo.py
+++ 20_por_ciento/02_conocer_estructuras_de_control/03_ciclo_while/03_while_anidado/01_conteo_simultaneo.py
@@ -6,3 +6,12 @@ Instrucciones:
 2.- Imprime las combinaciones posibles
 
 """
+
+i = 1
+while i <= 3:
+    j = 1
+    while j <= 3:
+        print(f" i = {i}, j = {j}")
+        j +=1
+    
+    i +=1
