684b0728d186ad14e6949ad5f85e1ccd1ccfb3dd
VictorCancino <juanvictorcancino@gmail.com>
2024-09-02 09:47:35 -0600

    Se agregarón las instrucciones del ejercicio

---
+++ 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for/10_usando_range/02_inicio_fin.py
@@ -0,0 +1,8 @@
+"""
+Nombre del ejercicio: Usar range con dos parametros
+
+Instrucciones: 
+1.- crea un ciclo for y has que imprima una cuenta entre dos número por ejemplo; entre el 7 y 15
+2.- Imprime las iteraciones
+
+"""
