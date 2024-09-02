c51e8f8df230ccd9de5ff0528a891b620b5e3286
VictorCancino <juanvictorcancino@gmail.com>
2024-09-02 09:55:41 -0600

    Se agregarón las instrucciones del ejercicio 'Usar range con saltos'

---
+++ 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for/10_usando_range/03_con_saltos.py
@@ -0,0 +1,9 @@
+"""
+Nombre del ejercicio: Usar range con saltos
+
+Instrucciones: 
+1.- Crea un ciclo for y usa un rango para crear una cuenta entre dos número por ejemplo; entre el 9 y 19
+2.- Has que dicha cuenta se realice con saltos entre cada numero iterado, por ejemplo de 3 en 3
+2.- Imprime las iteraciones
+
+"""
