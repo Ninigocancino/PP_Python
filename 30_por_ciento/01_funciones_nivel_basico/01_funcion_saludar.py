c8483110b56c01bc7e62636ccaff3e374e871155
VictorCancino <juanvictorcancino@gmail.com>
2024-09-18 21:49:57 -0600

    Se completó el ejercicio 'Función que saluda'

--- 30_por_ciento/01_funciones_nivel_basico/01_funcion_saludar.py
+++ 30_por_ciento/01_funciones_nivel_basico/01_funcion_saludar.py
@@ -4,3 +4,11 @@ Nombre del ejercicio: Función que saluda
 Instrucciones: 
 1.- Crea una función que imprima un saludo al ser llamada
 """
+
+def saludar():
+    #Esta función imprime un saludo
+    print("")
+    print("Hola, ¿Cómo estás?")
+    print("")
+
+saludar()
