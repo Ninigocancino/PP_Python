d0739130b299a500ecc72b3bb62cb09647cb0279
VictorCancino <juanvictorcancino@gmail.com>
2024-09-18 22:03:18 -0600

    Se completó el ejercicio 'Me presento'

--- 30_por_ciento/01_funciones_nivel_basico/02_me_presento.py
+++ 30_por_ciento/01_funciones_nivel_basico/02_me_presento.py
@@ -5,3 +5,13 @@ Instrucciones:
 1.- Crea una función que imprima un mensaje de presentación personal
 2.- Usa un parametro para agregar el nombre de una persona al mensaje
 """
+
+def soy(mi_nombre):
+
+    #Función que imprime un mensaje que incluye el nombre del usuario 
+
+    print("")
+    print(f"Hola, me llamo {mi_nombre} y estoy encantado de conocerte")
+    print("")
+
+soy("Hash")
