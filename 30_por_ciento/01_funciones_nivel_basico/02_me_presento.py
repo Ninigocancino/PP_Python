a4f9fbf76f4ca5187f3a75766c8dcf0a2460fb84
VictorCancino <juanvictorcancino@gmail.com>
2024-09-18 21:56:26 -0600

    Se agregarón las instrucciones al ajercicio 'Me presento'

---
+++ 30_por_ciento/01_funciones_nivel_basico/02_me_presento.py
@@ -0,0 +1,7 @@
+"""
+Nombre del ejercicio: Función que saluda
+
+Instrucciones: 
+1.- Crea una función que imprima un mensaje de presentación personal
+2.- Usa un parametro para agregar el nombre de una persona al mensaje
+"""
