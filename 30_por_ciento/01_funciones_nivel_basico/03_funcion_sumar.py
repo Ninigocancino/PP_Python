37ab9bc3dcab33b287764a9253395f0433874d63
VictorCancino <juanvictorcancino@gmail.com>
2024-09-18 22:13:36 -0600

    Se completó el ejercicio 'Operación suma'

--- 30_por_ciento/01_funciones_nivel_basico/03_funcion_sumar.py
+++ 30_por_ciento/01_funciones_nivel_basico/03_funcion_sumar.py
@@ -5,3 +5,10 @@ Instrucciones:
 1.- Crea una función que realice una operación suma
 2.- Pasa como parametros los valores a sumar
 """
+
+def suma(a,b):
+
+    sumatoria = a + b
+    print(sumatoria)
+
+suma(2,3)
