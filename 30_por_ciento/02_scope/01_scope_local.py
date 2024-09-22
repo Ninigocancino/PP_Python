ec86c7b71a1b929e23a59ce687b912f44c69cb2b
VictorCancino <juanvictorcancino@gmail.com>
2024-09-21 20:42:45 -0600

    Se agregarón las instrucciones del ejercicio 'Calcular área del circulo'

---
+++ 30_por_ciento/02_scope/01_scope_local.py
@@ -0,0 +1,8 @@
+"""
+Nombre del ejercicio: Calcular área del circulo
+
+Este ejercicio está enfocado en cómo se manejan las variables locales dentro de una función y cómo no son accesibles fuera de su contexto.
+
+Instrucciones: 
+1.- Intenta responder la pregunta ¿Qué sucede cuando intentas imprimir pi fuera de la función? antes de ejecutar el código
+"""
