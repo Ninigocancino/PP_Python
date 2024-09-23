1dbede0c175d69162e7629ec10d1aa700d9711a7
VictorCancino <juanvictorcancino@gmail.com>
2024-09-23 10:18:24 -0600

    Se completó el ejercicio 'Adivina el valor de X'

---
+++ 30_por_ciento/02_scope/03_scope_local_global.py
@@ -0,0 +1,17 @@
+"""
+Nombre del ejercicio: Adivina del valor de X
+
+En este ejercicio vas a trabajar con variables locales y globales. Intenta predecir qué imprimirá el código antes de ejecutarlo.
+
+Instrucciones: 
+1.- Crea una variable con un valor numérico
+2.- Crea dos funciones
+3.- En la primer función agrega una variable llamada igual que la variables creada fuera de la función, agrega un valor diferente y agrega la impresión de un mensaje que conteenga el valor de la variable
+4.- En la segunda función usa global para referenciar la variable global 
+5.- Modifica el valor de la variable después de referenciarla con global
+6.- Imprime un mensaje que contenga el valor de la cariable
+7.- Llama la funcion 1
+8.- Imprime un mensaje que incluya el valor de la variable después de la ejecución de la función 1
+9.- Llama la función 2
+10.- Imprime un mensaje que incluya el valor de la variable después de la ejecución de la función 2
+"""
