e6f6c0451343531e204c5feefebf6192ac2081f1
VictorCancino <juanvictorcancino@gmail.com>
2024-09-23 10:03:22 -0600

    Se completó el ejercicio 'Contador de llamadas'

--- 30_por_ciento/02_scope/02_scope_global.py
+++ 30_por_ciento/02_scope/02_scope_global.py
@@ -10,3 +10,23 @@ Instrucciones:
 4.- Llama a la función
 5.- Imprime un mensaje que imprima e valor final de la variable
 """
+
+contador_llamadas = 0  # Variable global
+
+print("")
+print(f"Contador antes de ejecutar función {contador_llamadas}")
+print("")
+
+def incrementar_contador():
+    global contador_llamadas  # Hacemos referencia a la variable global
+    contador_llamadas += 1
+    print(f"Contador dentro de la función: {contador_llamadas}")
+
+# Llamamos varias veces a la función
+incrementar_contador()
+incrementar_contador()
+incrementar_contador()
+
+# Verificamos el valor final de la variable global
+print("")
+print(f"Contador fuera de la función: {contador_llamadas}")
