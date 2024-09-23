6d427636ade849f4ef10990377e0884ba8b572c7
VictorCancino <juanvictorcancino@gmail.com>
2024-09-23 10:38:45 -0600

    Se agregarón las instrucciones del ejercicio 'Valor de Y'

---
+++ 30_por_ciento/02_scope/04_scope_nonlocal.py
@@ -0,0 +1,16 @@
+"""
+Nombre del ejercicio: Valor de Y
+
+En funciones anidadas (funciones dentro de funciones), las variables de la función externa no son locales, pero tampoco globales. Estas variables se encuentran en el scope nonlocal, y se pueden modificar usando la palabra clave nonlocal.
+
+Instrucciones: 
+1.- Crea una función
+2.- Crea una variable con valor numérico
+3.- Crea una función dentro de la función creada anteriormente (función anidada)
+4.- Dentro de la función interna referencia el valor de la variable creada en la función principal usando 'nonlocal' 
+5.- Modifica el valor de la variable desués de ser refernciada
+6.- Imprime un mensaje que contenga el valor de la variable ya referenciada
+7.- Dentro de la función principal llama la funcion interna
+8.- Dentro de la función principal imprime un mensaje que incluya el valor de la variable creada en la función principal
+9.- Fuera de la función principal llama a la función función principal
+"""
