bd3208aa87831e844ceea18046f1f27384889bfb
VictorCancino <juanvictorcancino@gmail.com>
2024-09-24 18:40:25 -0600

    Se completó el ejercicio 'Shadowing'

--- 30_por_ciento/02_scope/06_prueba_shadowing.py
+++ 30_por_ciento/02_scope/06_prueba_shadowing.py
@@ -6,3 +6,12 @@ Instrucciones:
 2.- Crea una función que defina una variable con el mismo nombre que la variable global. 
 
 """
+
+y = 200  # Variable global
+
+def prueba_shadowing():
+    y = 50  # Variable local que "sombra" la variable global
+    print(f"Valor de 'y' dentro de la función: {y}")
+
+prueba_shadowing()
+print(f"Valor de 'y' fuera de la función: {y}")
