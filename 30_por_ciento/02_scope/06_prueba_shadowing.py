ec479ab5b138137e440676820b8864b7e6558b0b
VictorCancino <juanvictorcancino@gmail.com>
2024-09-24 18:39:01 -0600

    Se agregarón las instrucciones del ejercicio 'Shadowing'

---
+++ 30_por_ciento/02_scope/06_prueba_shadowing.py
@@ -0,0 +1,8 @@
+"""
+Nombre del ejercicio: Shadowing
+
+Instrucciones: 
+1.- Crea una variable global
+2.- Crea una función que defina una variable con el mismo nombre que la variable global. 
+
+"""
