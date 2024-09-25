30451b8dfd4dc21462657133d3cf7a0b60327aec
VictorCancino <juanvictorcancino@gmail.com>
2024-09-24 19:40:30 -0600

    Se agregarón las instrucciones al ejercicio 'Convertir HEY en hey'

---
+++ 30_por_ciento/03_metedos_de_caracteres/01_mayuscula_minusculas/02_lower/01_palabra_min.py
@@ -0,0 +1,9 @@
+"""
+Nombre del ejercicio: Convertir HEY en hey
+
+Instrucciones: 
+1.- Crea una variable que contenga una cadena de caracteres en MAYÚSCULAS
+2.- Convierte la cadena en minúculas usando el método lower
+3.- Imprime el resultado
+
+"""
