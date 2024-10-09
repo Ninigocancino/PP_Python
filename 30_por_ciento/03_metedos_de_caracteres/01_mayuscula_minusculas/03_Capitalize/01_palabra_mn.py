23d099cc01e1c9e59a031424abfc0150c3ab6327
VictorCancino <juanvictorcancino@gmail.com>
2024-10-09 12:39:32 -0600

    Se completó el ejercicio 'Convierte hey en Hey'

---
+++ 30_por_ciento/03_metedos_de_caracteres/01_mayuscula_minusculas/03_Capitalize/01_palabra_mn.py
@@ -0,0 +1,13 @@
+"""
+Nombre del ejercicio: Convertir hey en Hey
+
+Instrucciones: 
+1.- Crea una variable que contenga una cadena de caracteres en minúsculas
+2.- Haz que la cadena en minúculas tenga la primer letra en mayucúla usando 'capitalize'
+3.- Imprime el resultado
+
+"""
+
+palabra = "saludo"
+
+print(palabra.capitalize())
