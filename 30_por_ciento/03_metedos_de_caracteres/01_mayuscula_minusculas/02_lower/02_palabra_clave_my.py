679026731bbfceb73eb421730d7977c19290ad90
VictorCancino <juanvictorcancino@gmail.com>
2024-10-09 11:45:36 -0600

    Se completó el ejercicio 'Palabra clave en mayúscula'

---
+++ 30_por_ciento/03_metedos_de_caracteres/01_mayuscula_minusculas/02_lower/02_palabra_clave_my.py
@@ -0,0 +1,13 @@
+"""
+Nombre del ejercicio: Plabra clave en Mayuscúla
+
+Instrucciones: 
+1.- Pide al usuario que ingrese una cadena de caracteres en MAYÚSCULAS
+2.- Convierte la cadena en minúculas usando el método lower en la impresión de la palabra
+3.- Imprime el resultado
+
+"""
+
+palabra_clave = input("Ingresa una palabra cualquiera: ")
+
+print(palabra_clave.lower())
