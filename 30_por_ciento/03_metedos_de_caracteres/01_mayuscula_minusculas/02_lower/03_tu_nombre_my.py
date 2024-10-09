feacacdc256cea7df8341daac885d8773f2a817a
VictorCancino <juanvictorcancino@gmail.com>
2024-10-09 11:53:59 -0600

    Se completó el ejercicio 'Tu nombre en mayúscula'

---
+++ 30_por_ciento/03_metedos_de_caracteres/01_mayuscula_minusculas/02_lower/03_tu_nombre_my.py
@@ -0,0 +1,13 @@
+"""
+Nombre del ejercicio: Tu nombre en mayúscula
+
+Instrucciones: 
+1.- Pide al usuario que ingrese su nombre en MAYÚSCULAS
+2.- Convierte el nombre del usuario en minúculas usando el método 'lower' en convinación con la función 'input'
+3.- Imprime el resultado
+
+"""
+
+tu_nombre = input("Ingresa tu nombre usandosolo mayúsculas: ").lower()
+
+print(tu_nombre)
