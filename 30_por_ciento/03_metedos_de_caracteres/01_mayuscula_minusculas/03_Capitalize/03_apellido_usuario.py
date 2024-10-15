029230c0ae8d125f766742e808feec895c593c2c
VictorCancino <juanvictorcancino@gmail.com>
2024-10-14 22:33:07 -0600

    Se competó el ejercicio 'Inicial Mayúcula Apellido'

---
+++ 30_por_ciento/03_metedos_de_caracteres/01_mayuscula_minusculas/03_Capitalize/03_apellido_usuario.py
@@ -0,0 +1,13 @@
+"""
+Nombre del ejercicio: Inicial Mayúscula Apellido
+
+Instrucciones: 
+1.- Pide al usuario que ingrese su nombre apellido en minúsculas
+2.- Convierte la primer letra de la cadena ingresada por el usuario de minúscula a mayúscula usando capitalize desde la variable
+3.- Imprime el resultado
+
+"""
+
+apellido_s = input("Ingresa tu apellido en minúculas por favor: ").capitalize()
+
+print(apellido_s)
