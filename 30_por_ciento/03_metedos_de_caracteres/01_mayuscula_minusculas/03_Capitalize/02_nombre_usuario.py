42f4d7133fbed17eb152ce11ad6f4ac71c7157a8
VictorCancino <juanvictorcancino@gmail.com>
2024-10-14 22:27:53 -0600

    Se Completó el ejercicio 'Inicial Mayúscula'

---
+++ 30_por_ciento/03_metedos_de_caracteres/01_mayuscula_minusculas/03_Capitalize/02_nombre_usuario.py
@@ -0,0 +1,13 @@
+"""
+Nombre del ejercicio: Inicial Mayúscula
+
+Instrucciones: 
+1.- Pide al usuario que ingrese su nombre
+2.- Convierte la primer letra de la cadena ingresada por el usuario de minúscula a mayúscula usando capitalize en el print
+3.- Imprime el resultado
+
+"""
+
+nombre_s = input("Ingresa tu nombre en minúculas por favor: ")
+
+print(nombre_s.capitalize())
