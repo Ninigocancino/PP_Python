dd4de5f6b7cdfafcf9bcbf25f52e5d6f2346290f
VictorCancino <juanvictorcancino@gmail.com>
2024-10-14 22:45:00 -0600

    Se completó el ejercicio 'Título libro'

---
+++ 30_por_ciento/03_metedos_de_caracteres/01_mayuscula_minusculas/04_title/01_titulo_facil.py
@@ -0,0 +1,13 @@
+"""
+Nombre del ejercicio: Título libro
+
+Instrucciones: 
+1.- Crea una variable que guarde el título de un libro que contenga más de una palabra
+2.- Convierte la primer letra de cada palabra del título usando el método title
+3.- Imprime el título 
+
+"""
+
+libro_t = "repensar la pobreza"
+
+print(libro_t.title())
