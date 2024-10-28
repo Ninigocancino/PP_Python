da807df70a097e381430ef178db7b19cc58d0362
VictorCancino <juanvictorcancino@gmail.com>
2024-10-28 14:32:48 -0600

    Se agregarón las instrucciones del proyecto '05_tu_peso_x_astro_v1.py'

---
+++ 10_por_ciento/Soluciones/03_proyectos/05_tu_peso_x_astro_v1.py
@@ -0,0 +1,29 @@
+"""
+INFORMACIÓN DEL PROYECTO:
+
+Nombre del Proyecto: Tu peso en un astro
+
+Descripción:
+Progamaremos un juego que permita calcular el peso de una persona en la luna 
+
+
+Instrucciones:
+Se requiere que el proyecto se aborde por bloques de funcionalidades para poder detectar errores e incorporar mejoras de forma más ágil y esbelta.
+
+Caso de uso:
+Se requiere desarrollar un juego para una clase escolar donde los alumnos tengan un primer acercamiento al tema de la gravedad y como afecta esta el peso de los objetos en diferentes cuerpos celestes. En este caso se pretende que el programa ayude a los alumnos conocer la diferencia entre su peso en la tierra y su peso hipotetico en la luna. 
+
+Historia de usuario:
+Cómo profesor de la clase requiero un programa que permita a mis alumnos ingresar el peso de un objeto en la tierra para conocer el precio estimado del mismo objeto si estuviera en la luna, además de que proporcione información de como se realizó el calculo. 
+
+
+Criterios de calidad:
+Crear un programa sencillo que realice las siguientes acciones: 
+1.- El programa debe imprimir un mensaje de bienvenida
+2.- El programa debe debe imprimir una descripción del programa
+3.- El programa debe debe imprimir al menos dos de las instrucciones del programa
+4.- Los textos deben mostrarse de forma amigable para facilitar al usuario su lectura 
+5.- El programa debe mostrar al usuario un ejemplo sencillo de la utilidad que tendrá el programa, mostrando la conversión del peso del objeto en la tierra al peso estimado del mismo objeto pero en la luna.
+6.- El programa debe mostrar la explicación de la operación
+7.- El programa debe mostrar un mensaje de despedida antes de cerrarse
+"""
