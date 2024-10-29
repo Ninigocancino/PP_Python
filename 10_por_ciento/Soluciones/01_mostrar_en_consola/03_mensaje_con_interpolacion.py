773088cf5d6edcd7b9dc35c07dc1620194b5a637
VictorCancino <juanvictorcancino@gmail.com>
2024-10-29 15:51:20 -0600

    Se corrigió un error ortográfico en el ejercicio 03 del directorio '01_mostrar en consola'

--- 10_por_ciento/Soluciones/01_mostrar_en_consola/03_mensaje_con_interpolacion.py
+++ 10_por_ciento/Soluciones/01_mostrar_en_consola/03_mensaje_con_interpolacion.py
@@ -1,8 +1,9 @@
 """
 Nombre del ejercicio: Paso 2
 
-Instrucciones: Crea una variable que guarde el valor string 'Estoy listo' y otra que guarde el valor 'paso 2', después usa la función 'print' para imprimir el mensaje 'Estoy listo para el paso dos en Pytho' en consola.
+Instrucciones: Crea una variable que guarde el valor string 'Estoy listo' y otra que guarde el valor 'paso 2', después usa la función 'print' para imprimir el mensaje 'Estoy listo para el paso dos en Python' en consola.
 """
+
 
 parte_1 = "Estoy listo"
 
