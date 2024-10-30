617fc09c7d8d79fd8c128d8050664d1095d1a257
VictorCancino <juanvictorcancino@gmail.com>
2024-10-30 08:07:48 -0600

    Se completó el ejercicio 'Lista deletreo'

---
+++ 20_por_ciento/01_conocer_las_listas/01_creacion_de_listas/04_deletrear.py
@@ -0,0 +1,10 @@
+"""
+Nombre del ejercicio: Lista deletreo
+
+Instrucciones: Crea piensa en un nombre con un apellido, luego crea una lista donde cada letra sea un elemento de lista y por último imprime la lista en consola
+"""
+
+palabra = ["A", "L", "L", "A", "N", " ", "T", "U", "R", "I", "N", "G"]
+
+print(palabra)
+
