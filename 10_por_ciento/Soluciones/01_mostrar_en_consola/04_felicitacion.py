2369fcd2c1219187ff29a78a79b1b80b8e1caf59
VictorCancino <juanvictorcancino@gmail.com>
2024-10-29 22:01:38 -0600

    Se completó el ejercicio 'felicitación'

---
+++ 10_por_ciento/Soluciones/01_mostrar_en_consola/04_felicitacion.py
@@ -0,0 +1,12 @@
+"""
+Nombre del ejercicio: felicitación
+
+Instrucciones: Crea una mensaje de felicitación uniendo cautro valores string guardados en sus respectivas variables, luego impirme el mensaje usnato interpolación.
+"""
+
+f1 = "Felicidades"
+nombre = "Mary"
+f2 = "Hoy es un gran día para ti"
+f3 = "y espero tengas muchos exitos más"
+
+print(f"{f1} {nombre}, {f2} {f3}")
