675f27b2fde9397b1081ec7f33da3585df303307
VictorCancino <juanvictorcancino@gmail.com>
2024-10-29 22:15:32 -0600

    Se completó el ejercicio'Calcular el porcentaje de una cifra'

---
+++ 10_por_ciento/Soluciones/02_operaciones/09_calcular_percentil.py
@@ -0,0 +1,13 @@
+"""
+Nombre del ejercicio: Calcular el porcentaje de una cifra
+
+Instrucciones: Crea un programa que calcule un porcentaje determinado, almacenado en una variable, de una cantidad total almacenado en otra variable, luego imprime el resultado con un ensaje descriptivo
+"""
+
+cantidad = 50
+
+porcentaje = 5
+
+r = porcentaje / cantidad *100
+
+print(f"el {porcentaje}% de {cantidad} es {r}")
