46f9a5aa1b1fa2c6e7067826fb19f3a3d82e3489
VictorCancino <juanvictorcancino@gmail.com>
2024-11-13 17:25:04 -0600

    Se completó el ejercicio 'imprimir titulos'

---
+++ 20_por_ciento/02_conocer_estructuras_de_control/02_ciclo_for/01_recorrer_e_imprimir/06_imprimir_titulos.py
@@ -0,0 +1,13 @@
+
+
+archivo = ["Hyperfocus", "El Quijote", "Originals", "Romeo y Julieta", "Sapiens"]
+
+print("")
+print("Libros en el archivo")
+print(archivo)
+print("")
+
+print("Lista de libros disponibles:")
+
+for libro in archivo:
+    print(libro)
