b2a774f59521c96430953340851190218cafad8b
VictorCancino <juanvictorcancino@gmail.com>
2024-11-12 22:31:51 -0600

    Se completó el ejercicio 'Lista libros'

---
+++ 20_por_ciento/01_conocer_las_listas/01_creacion_de_listas/05_lista_libros.py
@@ -0,0 +1,9 @@
+"""
+Nombre del ejercicio: Crea una lista de libros
+
+Instrucciones: Crea una lista con  5 titulos de libros y luego imprime la lista
+"""
+
+libros = ["Hyperfocus", "Fueras de serie", "Originals", "Repensar la pobreza", "Esencialismo"]
+
+print(libros)
