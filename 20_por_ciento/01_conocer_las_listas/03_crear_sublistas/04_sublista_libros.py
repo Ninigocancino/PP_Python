0cbc4e197a55efde4b5acf700ccc1e883c6ee01f
VictorCancino <juanvictorcancino@gmail.com>
2024-11-19 19:38:31 -0600

    Se completó el ejercicio 'sublista libros'

---
+++ 20_por_ciento/01_conocer_las_listas/03_crear_sublistas/04_sublista_libros.py
@@ -0,0 +1,18 @@
+
+
+libros = ["Hyperfocus","Originals", "Repensar la pobreza","Sapeins", "Esencialismo", "Fuera de serie", "El cisne negro", "Small data", "Así se domina el mundo"]
+
+conteo = len(libros)
+
+print("")
+print(f"Existen {conteo} elementos en la lista")
+
+print("")
+libros_1 = libros[0:4]
+libros_2 = libros[4:8]
+
+print("Sublista 01:")
+print(libros_1)
+print("")
+print("Sublista 02: ")
+print(libros_2)
