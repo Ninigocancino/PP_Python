5f3e776a49c2fa5fabc1aef9403c1bb895fd22ae
VictorCancino <juanvictorcancino@gmail.com>
2024-12-13 21:58:38 -0600

    Se creó un comentario multilinea en el fichero 'nota_para_ver.py'

---
+++ 20_por_ciento/01_conocer_las_listas/01_creacion_de_listas/nota_para_ver.py
@@ -0,0 +1,3 @@
+"""
+Crear ejercicios tematicos para todo el repositorio sobre temas ralacionados con cosas interesantes como premios nobles, datos de planteas, datos historicos, etc. con la intensión de dotar de cierta utilidad real o relacionada con el mundo a este repositorio
+"""
