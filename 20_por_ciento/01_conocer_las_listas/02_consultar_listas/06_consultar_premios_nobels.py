88dd4f8d550e17cbd5c74c6fe802fc963bd3e2cf
VictorCancino <juanvictorcancino@gmail.com>
2024-12-14 23:25:02 -0600

    Se completó el ejercicio 'consultar lista Premios Nobels'

---
+++ 20_por_ciento/01_conocer_las_listas/02_consultar_listas/06_consultar_premios_nobels.py
@@ -0,0 +1,12 @@
+
+categorias = ["Fisica", "Quimica", "Medicina", "Literatura", "Paz", "Economía"]
+
+num_categorias = len(categorias)
+
+print(num_categorias)
+
+posicion_3 = categorias[3]
+posicion_4 = categorias[4]
+
+print(posicion_3)
+print(posicion_4)
