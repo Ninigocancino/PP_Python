3e6857222f280774960ae1a912983d17fc61e293
VictorCancino <juanvictorcancino@gmail.com>
2024-12-15 00:01:40 -0600

    Se completó el ejercicio 'sublista planetas'

---
+++ 20_por_ciento/01_conocer_las_listas/03_crear_sublistas/07_sublista_planetas.py
@@ -0,0 +1,9 @@
+
+
+planetas = ["Mercurio","Venus","Tierra","Marte","Jupiter", "Urano", "Neptuno","Kepler-90b","Kepler-90c","Kepler-90d","Kepler-90e","Kepler-90f","Kepler-90g","Kepler-90h","Kepler-90i"]
+
+sistema_solar = planetas[0:7]
+Kepler = planetas[7:]
+
+print(sistema_solar)
+print(Kepler)
