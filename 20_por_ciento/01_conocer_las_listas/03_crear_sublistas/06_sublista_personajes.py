c8717cf7f068290a34b60d44683e9486437de548
VictorCancino <juanvictorcancino@gmail.com>
2024-12-14 23:54:00 -0600

    Se completó el ejercicio 'Sublista personajes'

---
+++ 20_por_ciento/01_conocer_las_listas/03_crear_sublistas/06_sublista_personajes.py
@@ -0,0 +1,9 @@
+
+
+personajes = ["Van Gogh", "Miro", "Mone", "Sezane", "Warhol", "Einstein", "Bohr", "Heisenberg", "Curie", "Feynman","Fermi"]
+
+artistas = personajes[0:5]
+fisicos = personajes[5:]
+
+print(artistas)
+print(fisicos)
