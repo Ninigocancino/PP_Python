e1d43d7ea6af71d4e8f1ce9583bc60760a3ae5b3
VictorCancino <juanvictorcancino@gmail.com>
2024-12-15 18:48:10 -0600

    Se completó el ejercicio 'Trivia premio Nobel'

---
+++ 20_por_ciento/02_conocer_estructuras_de_control/01_if_else/07_premio_nobel.py
@@ -0,0 +1,11 @@
+
+print("Responde la pregunta")
+
+pregunta = input("¿Cuantas categorías tiene el premio Nobel?: ")
+
+print("")
+
+if pregunta == "5":
+    print("Correcto en el premio Nobel se reconoce a lo más destacado en 1)Fisica, 2)Quimica, 3)Literatura, 4)Economía y 5)la Paz")
+else:
+    print("La respuesta no es correcta, intenta de nuevo")
