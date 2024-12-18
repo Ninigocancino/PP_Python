31aab631f3ff5f3a67a26e4ac50a87ef1a9537ae
VictorCancino <juanvictorcancino@gmail.com>
2024-12-17 21:16:53 -0600

    Se completó el ejericio 'Acceder a tupla multidatos'

---
+++ 20_por_ciento/02_conocer_las_tuplas/02_acceder_a_elementos_de_tuplas/02_acceder_multidatos.py
@@ -0,0 +1,13 @@
+
+datos= ('String',1,True, '2')
+
+dato_string=datos[0]
+dato_numero = datos[1]
+dato_bool = datos[2]
+numero_string = datos[3]
+
+print(dato_string)
+print(dato_numero)
+print(dato_bool)
+print(numero_string)
+
