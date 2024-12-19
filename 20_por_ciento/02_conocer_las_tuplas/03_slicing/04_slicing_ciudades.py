70369987a4124683ab76f6e5dc7a4d72fd6c514e
VictorCancino <juanvictorcancino@gmail.com>
2024-12-19 14:49:17 -0600

    Se completó el ejercicio 'Hacer slicing en la lista ciudades'

---
+++ 20_por_ciento/02_conocer_las_tuplas/03_slicing/04_slicing_ciudades.py
@@ -0,0 +1,24 @@
+
+ciudades = 'Toronto','Otawa','Los Angenles','Miami','San Francisco','Whasinton','México','Villahermosa','Merida','Cancún','Monterrey','Puebla', 'Caracas','Medellín','Bogota','Carteagena','Madrid','Barcelona','Bilbao','Zaragosa','Valencia'
+
+print(len(ciudades))
+
+canada = ciudades[0:2]
+eua = ciudades[2:6]
+mexico = ciudades[6:12]
+venezuela = ciudades[12:13]
+colombia = ciudades[13:16]
+españa = ciudades[16:22]
+españa2 = ciudades[16:]
+españa3 = ciudades[16:21]
+españa4= ciudades[16:20]
+
+print(f"Canada => {canada}")
+print(f"EUA => {eua}")
+print(f"México => {mexico}")
+print(f"Venezuela => {venezuela}")
+print(f"Colombía => {colombia}")
+print(f"España => {españa}")
+print(f"España => {españa2}")
+print(f"España => {españa3}")
+print(f"España => {españa4}")
