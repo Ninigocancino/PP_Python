b8aa6ed8322437d7193fa95674e3e63a7da6ce52
VictorCancino <juanvictorcancino@gmail.com>
2024-12-19 14:25:28 -0600

    Se completó el ejercicio 'Hacer slicing en la lista libros'

---
+++ 20_por_ciento/02_conocer_las_tuplas/03_slicing/03_slicing_libro.py
@@ -0,0 +1,13 @@
+
+
+libros = 'El cisne negro', 'Fuera de series', '100 años de soledad', 'El Quijote de la Mancha', 'Originales', 'El ADN de la Innovación', 'Así se Domina el Mundo'
+
+conteo = len(libros)
+print(conteo)
+
+
+fraccion = libros[0:5]
+print(fraccion)
+print(libros[4:6])
+print(libros[5:6])
+print(libros[4:])
