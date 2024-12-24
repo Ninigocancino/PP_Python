d2e321f8f44b2fffdeb5cf574b3dcc97d9258913
VictorCancino <juanvictorcancino@gmail.com>
2024-12-24 16:45:10 -0600

    Se completó el ejercicio 'Hacer slicing con salto en la tupla libros'

---
+++ 20_por_ciento/02_conocer_las_tuplas/04_slicing_con_salto/03_ss_libros.py
@@ -0,0 +1,12 @@
+
+libros= ('HyperFocus','Repensar la pobreza','Fueras de serie','Sapiens','El quijote de la Mancha','Cien años de soledad','El cisne negro','El Dominio Mundial')
+
+print(len(libros))
+
+s_libros = libros[2:5]
+ss_libros = libros[1::3]
+ssi_libros = libros[::-3]
+
+print(s_libros)
+print(ss_libros)
+print(ssi_libros)
