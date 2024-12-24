bbb6112266a294c07ed2df765adc84884a3d45a8
VictorCancino <juanvictorcancino@gmail.com>
2024-12-23 18:25:10 -0600

    Se completó el ejerciico 'Hacer slicing a la lista ganadores nobl'

---
+++ 20_por_ciento/02_conocer_las_tuplas/03_slicing/05_slicing_nobel.py
@@ -0,0 +1,14 @@
+
+ganadores_nobel = "Albert Einstein","Niels Bohr","Richard Feynman","Stephen Hawking","Roger Penrose","Marie Curie","Linus Pauling","Dorothy Crowfoot Hodgkin","Frederick Sanger","Ada Yonath","Frances Arnold","Alexander Fleming","Jonas Salk","Barbara McClintock","James D. Watson","Susumu Tonegawa","Tu Youyou","Rabindranath Tagore","William Faulkner","Gabriel García Márquez","Toni Morrison","Bob Dylan","Olga Tokarczuk","Nelson Mandela","Madre Teresa de Calcuta","Malala Yousafzai","Barack Obama","Denis Mukwege","Tawakkol Karman"
+
+fisica = ganadores_nobel[0:6]
+quimica = ganadores_nobel[5:11]
+medicina = ganadores_nobel[11:17]
+literatura = ganadores_nobel[17:23]
+paz = ganadores_nobel[23:]
+
+print(fisica)
+print(quimica)
+print(medicina)
+print(literatura)
+print(paz)
