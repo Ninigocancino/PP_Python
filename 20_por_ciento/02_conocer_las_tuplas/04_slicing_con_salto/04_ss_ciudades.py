263843910fb1ce9ee8df99c55dace876f5794c49
VictorCancino <juanvictorcancino@gmail.com>
2024-12-26 21:16:00 -0600

    Se completó el ejercicio 'hacer slicing con salto inverso a la tupla ciudades'

---
+++ 20_por_ciento/02_conocer_las_tuplas/04_slicing_con_salto/04_ss_ciudades.py
@@ -0,0 +1,12 @@
+
+ciudades = 'Toronto','Otawa','Los Angenles','Miami','San Francisco','Whasinton','México','Villahermosa','Merida','Cancún','Monterrey','Puebla', 'Caracas','Medellín','Bogota','Carteagena','Madrid','Barcelona','Bilbao','Zaragosa','Valencia',"Murcia"
+
+
+c_ciudades=len(ciudades)
+
+s_ciudades = ciudades[0::3]
+corte_2 = ciudades[::-3]
+
+print(c_ciudades)
+print(s_ciudades)
+print(corte_2)
