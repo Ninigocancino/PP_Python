d8b7acf4728f9e226fb3b578baff57577ab8c22b
VictorCancino <juanvictorcancino@gmail.com>
2025-01-02 20:05:28 -0600

    Se completó el ejercicio 'Crear diccionario con información de libros'

---
+++ 20_por_ciento/03_conocer_los_diccionarios/01_crear-diccionarios/04_info_libros.py
@@ -0,0 +1,12 @@
+
+libros_info = {
+    'Titulo': 'Sapiens, De animales a dioses',
+    'Autor': 'Yuval Noah Harari',
+    'Año': '2016',
+    'N_paginas': 450,
+    'Edición': '1era edición',
+    'Genero':'Pensamiento, filosofía',
+    'Precio': '$650 mxn'
+}
+
+print(libros_info)
