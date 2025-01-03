47cd99328b2f1328c37ce1279576cea22cca2630
VictorCancino <juanvictorcancino@gmail.com>
2025-01-02 20:17:33 -0600

    Se completó el ejercicio 'Crear diccionario de ganadores del premio nobel de 2024'

---
+++ 20_por_ciento/03_conocer_los_diccionarios/01_crear-diccionarios/05_premios_nobels.py
@@ -0,0 +1,11 @@
+
+ganadores_2024 = {
+    'Literatura': 'Han Kang',
+    'Fisica' : 'Geoffri Hinton, John Holpfield',
+    'Medicina': 'Victor Ambros, Garu Ruvkun',
+    'Economía': 'James A. Robinson, Simon Johnson, Daron Acemoglu',
+    'Paz' : 'Nihon Hidankyo',
+    'Quimica': 'Demis Hassabis, David Baker, John Jumper'
+}
+
+print(ganadores_2024)
