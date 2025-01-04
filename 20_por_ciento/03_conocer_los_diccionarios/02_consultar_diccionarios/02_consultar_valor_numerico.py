d332bee4bdda0d23fab239abe07520f1ab4bd214
VictorCancino <juanvictorcancino@gmail.com>
2025-01-03 21:28:24 -0600

    Se completó el ejercicio 'Acceder al valor de la clave 'cuartenta' '

---
+++ 20_por_ciento/03_conocer_los_diccionarios/02_consultar_diccionarios/02_consultar_valor_numerico.py
@@ -0,0 +1,13 @@
+
+numero_valor = {
+    'veinte': 20,
+    'treinta':30,
+    'cuarenta': 40,
+    'cincuenta': 50,
+}
+
+print("")
+print(numero_valor)
+print("")
+print("Acceder al valor de la clave 'cuarenta': ")
+print(numero_valor['cuarenta'])
