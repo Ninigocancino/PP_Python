cc5c6201d7a4f1344e57ccf97285cae33aca2640
VictorCancino <juanvictorcancino@gmail.com>
2025-01-03 21:46:07 -0600

    Se completó el ejercicio 'Acceder a la clave 'Fisica' en el diccionario ganadores_2024'

---
+++ 20_por_ciento/03_conocer_los_diccionarios/02_consultar_diccionarios/05_consultar_premios_nobel.py
@@ -0,0 +1,17 @@
+
+ganadores_2024 = {
+    'Literatura': 'Han Kang',
+    'Fisica' : 'Geoffri Hinton, John Holpfield',
+    'Medicina': 'Victor Ambros, Garu Ruvkun',
+    'Economía': 'James A. Robinson, Simon Johnson, Daron Acemoglu',
+    'Paz' : 'Nihon Hidankyo',
+    'Quimica': 'Demis Hassabis, David Baker, John Jumper'
+}
+
+print(ganadores_2024)
+
+acceder = ganadores_2024['Fisica']
+
+print("")
+print("Accede a la clave 'Fisica': ")
+print(acceder)
