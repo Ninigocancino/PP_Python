a639b568d4d5f80e1618326371f1a019ef11202d
VictorCancino <juanvictorcancino@gmail.com>
2025-01-03 21:42:36 -0600

    Se completó el ejercicio 'Acceder a la clave 'Edicion' en el diccionario libros_info

---
+++ 20_por_ciento/03_conocer_los_diccionarios/02_consultar_diccionarios/04_consultar_info_libros.py
@@ -0,0 +1,16 @@
+
+libros_info = {
+    'Titulo': 'Sapiens, De animales a dioses',
+    'Autor': 'Yuval Noah Harari',
+    'Año': '2016',
+    'N_paginas': 450,
+    'Edicion': '1era edición',
+    'Genero':'Pensamiento, filosofía',
+    'Precio': '$650 mxn'
+}
+
+print("")
+print(libros_info)
+print("")
+print("Accede a la clave 'Edición': ")
+print(libros_info['Edicion'])
