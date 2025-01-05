0bfa87b04c227d1f64441ef1b27658b0301a479d
VictorCancino <juanvictorcancino@gmail.com>
2025-01-04 23:30:41 -0600

    Se completó el ejercicio 'Consultar el valor de la clave 'Galaxia' en el diccionario 'Planeta'

---
+++ 20_por_ciento/03_conocer_los_diccionarios/02_consultar_diccionarios/06_consultar_planetas.py
@@ -0,0 +1,13 @@
+
+planeta = {
+    'Nombre':'Tierra',
+    'Gravedad': '1g',
+    'Galaxia': 'Sistema Solar',
+    'Tipo orbita': 'Elipsoide'
+}
+
+valor= planeta['Galaxia']
+print(planeta)
+print("")
+print("Muestra el valor de la clave 'Galaxia': ")
+print(valor)
