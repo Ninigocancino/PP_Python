ea0134c42e472df9812ffdf287f3d7da7af9ac91
VictorCancino <juanvictorcancino@gmail.com>
2025-01-08 22:38:18 -0600

    Se agregó un nuevo par clave-valor al diccionario 'ganadores_2024'

---
+++ 20_por_ciento/03_conocer_los_diccionarios/03_modificar_diccionarios/05_nobel.py
@@ -0,0 +1,21 @@
+
+ganadores_2024 = {
+    'Literatura': 'Han Kang',
+    'Fisica' : 'Geoffri Hinton, John Holpfield',
+    'Medicina': 'Victor Ambros, Garu Ruvkun',
+    'Economía': 'James A. Robinson, Simon Johnson, Daron Acemoglu',
+    'Paz' : 'Nihon Hidankyo',
+    'Quimica': 'Demis Hassabis, David Baker, John Jumper'
+}
+
+print("")
+print("Diccionario original")
+print(ganadores_2024)
+print("")
+
+print("")
+print("Agregar un nuevo par clave-valor")
+ganadores_2024['Matematicas']="Esta no es una categoría de Nobel"
+print(ganadores_2024)
+
+print("")
