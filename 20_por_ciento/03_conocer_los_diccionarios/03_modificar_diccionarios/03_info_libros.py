4619897223703def5f17c6fe03b08d4bfec540d7
VictorCancino <juanvictorcancino@gmail.com>
2025-01-08 21:17:11 -0600

    Se modificó el valor de la clave 'N_paginas' en el dicionario 'libros_info' perteneciente al ejercicio 'Modificar diccionario con información de libros'

---
+++ 20_por_ciento/03_conocer_los_diccionarios/03_modificar_diccionarios/03_info_libros.py
@@ -0,0 +1,20 @@
+
+libros_info = {
+    'Titulo': 'Sapiens, De animales a dioses',
+    'Autor': 'Yuval Noah Harari',
+    'Año': '2016',
+    'N_paginas': 450,
+    'Edición': '1era edición',
+    'Genero':'Pensamiento, filosofía',
+    'Precio': '$650 mxn'
+}
+
+print("")
+print("Diccionario original")
+print(libros_info)
+print("")
+
+print("")
+print("Modificar el valor de la clave 'N_paginas' ")
+libros_info['N_paginas'] = 600
+print(libros_info)
