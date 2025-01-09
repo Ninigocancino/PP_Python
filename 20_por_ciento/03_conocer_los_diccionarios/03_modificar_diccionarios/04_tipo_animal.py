9ab7490f4d5cccd4502b4d565ba396768f8624b7
VictorCancino <juanvictorcancino@gmail.com>
2025-01-08 21:40:34 -0600

    Se agregó un nuevo par clave-valor al diccionario 'tipo_animal' del ejercicio 'Modificar el diccionario tipo_animal'

---
+++ 20_por_ciento/03_conocer_los_diccionarios/03_modificar_diccionarios/04_tipo_animal.py
@@ -0,0 +1,18 @@
+
+tipo_animal = {
+    'Pato': 'Acuatico, Terrestre, Volador',
+    'Perro' : 'Terrestre',
+    'Trucha': 'Acuatico',
+    'Murcielago': 'Volador'
+}
+
+print("")
+print("Diccionario original")
+print(tipo_animal)
+print("")
+
+print("Agregar un nuevo par clave-valor")
+tipo_animal['Delfin']= 'Acuatico'
+print(tipo_animal)
+
+print("")
