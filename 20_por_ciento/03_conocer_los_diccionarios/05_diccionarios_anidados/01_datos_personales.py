e3a801de68c2f0bf78e19ba7d9a2b7623f7463b8
VictorCancino <juanvictorcancino@gmail.com>
2025-01-10 19:34:37 -0600

    Se completó el ejericcio crear un diciconario anidado con datos de personas

---
+++ 20_por_ciento/03_conocer_los_diccionarios/05_diccionarios_anidados/01_datos_personales.py
@@ -0,0 +1,18 @@
+
+datos = {
+    'María': {'Edad':23, 'Apellido_Paterno': 'Gomez', 'Apellido_materno': 'Salas', 'Ciudad':'Cuernavaca'},
+    'John' : {'Edad':35, 'Apellido_Paterno': 'Smith', 'Apellido_Materno': '', 'Ciudad': 'Portland'},
+    'Tiago' : {'Edad': 28, 'Apellido_Paterno': 'Silva', 'Apellido_Materno': 'Do Santo', 'Ciudad': 'Río De Janeiro'}
+}
+
+print("")
+print("Diccionario:")
+print(datos)
+
+print("")
+
+maria = datos['María']
+
+print(maria)
+
+print("")
