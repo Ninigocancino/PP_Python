7e1d8f97f39f6e279637d6baf3c86bc12614e017
VictorCancino <juanvictorcancino@gmail.com>
2025-01-14 23:12:57 -0600

    Se creó el diccionario 'libros' para trabajar el ejercicio 'Agregar multiples valores a las claves de un diccionario sobre libros'

---
+++ 20_por_ciento/03_conocer_los_diccionarios/03_crear_listas_de_diccionarios/04_Ldict-info_libros.py
@@ -0,0 +1,10 @@
+
+libros = {
+    'titulos' : ['Sapiens','Hyperfocus','Así se domina el mundo'],
+    'autores' : ['Yuval Noha Harari','Chris Bailey', 'Pedro Baños']
+}
+
+print()
+print("Diciconario: libros")
+print(libros)
+print()
