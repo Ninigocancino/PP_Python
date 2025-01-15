e60085f9088fedc1630ec48bfd23a3763e533952
VictorCancino <juanvictorcancino@gmail.com>
2025-01-14 22:56:09 -0600

    Se creó el diccionario 'animales' y se agregarón multiples valores a sus claves usando listas

---
+++ 20_por_ciento/03_conocer_los_diccionarios/03_crear_listas_de_diccionarios/03_Ldict_animales.py
@@ -0,0 +1,9 @@
+
+animales = {
+    'Reptiles' : ['Boa','Cocodrilo','Tortuga','Lagarto'],
+    'Mamiferos' : ['Perro','Gato','Lobo','León'],
+    'Anfibios' : ['Rana','Sapo']
+}
+
+print("")
+print(animales)
