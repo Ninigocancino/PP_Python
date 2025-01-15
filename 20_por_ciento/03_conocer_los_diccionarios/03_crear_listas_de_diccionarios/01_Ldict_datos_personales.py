c322037c9be2c969c6039124772b2f7c563a6348
VictorCancino <juanvictorcancino@gmail.com>
2025-01-14 22:41:32 -0600

    Se imprimierón los valores de las claves del diccionario

--- 20_por_ciento/03_conocer_los_diccionarios/03_crear_listas_de_diccionarios/01_Ldict_datos_personales.py
+++ 20_por_ciento/03_conocer_los_diccionarios/03_crear_listas_de_diccionarios/01_Ldict_datos_personales.py
@@ -5,4 +5,14 @@ datos = {
     'ciudad' : ['Dubai','Tokyo','Viena']
 }
 
+print("")
 print(datos)
+print()
+
+print("Acceder a los valores de las claves")
+nombres=datos['nombres']
+print(nombres)
+print(datos['edad'])
+ciudad = datos['ciudad']
+print(ciudad)
+print("")
