ac6ae0b7a1580868743c49a7e2efe12df6039187
VictorCancino <juanvictorcancino@gmail.com>
2025-01-15 21:42:00 -0600

    Se completó la tarea 'Crear el diccionario numeros usando listas como valores de las claves'

---
+++ 20_por_ciento/03_conocer_los_diccionarios/03_crear_listas_de_diccionarios/02_Ldict_numeros.py
@@ -0,0 +1,14 @@
+
+
+
+numeros = {
+    'pares' : [2,4,6,8,10,12],
+    'primos' : [2,3,5,7,11,13],
+    'impares' : [1,3,5,7,9,11],
+    'romanos_pares' : ['II','IV','VI','VIII','X','XII']
+}
+
+print("")
+print("Diccionario números manejando listas como valres de las claves")
+print(numeros)
+print()
