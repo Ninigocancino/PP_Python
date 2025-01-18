fbb9fd7a144ffa2039298fc5ba473941bb30a253
VictorCancino <juanvictorcancino@gmail.com>
2025-01-17 21:09:22 -0600

    Se agregó el diccionario 'planetas' con listas como valores al fichero '06_Ldict_planetas.py'

--- 20_por_ciento/03_conocer_los_diccionarios/03_crear_listas_de_diccionarios/06_Ldict_planetas.py
+++ 20_por_ciento/03_conocer_los_diccionarios/03_crear_listas_de_diccionarios/06_Ldict_planetas.py
@@ -1,1 +1,7 @@
 #Manipular el diccionario planeta cuando tiene listas como valores
+
+planetas = {
+    'nombre' : ['Jupiter','Saturno','Urano','Neptuno','Tierra','Venus','Marte','Mercurio'],
+    'tamaño' : ['142,984','108,728 ','51,118','49,532','12,756','12,104','6,794','4,880'],
+    'p_elemento' : ['Hidrogeno','Hidrogeno','Hidrogeno','Hidrogeno','Oxigeno','Dioxido de carbono','Hierro','Hierro']
+}
