4f18041a1a3739a810f915a278972dd7fe675c82
VictorCancino <juanvictorcancino@gmail.com>
2025-01-18 23:15:29 -0600

    Se creó el diccionario 'premios_nobel' con listas como valores

--- 20_por_ciento/03_conocer_los_diccionarios/03_crear_listas_de_diccionarios/05_Ldict_premios_nobel.py
+++ 20_por_ciento/03_conocer_los_diccionarios/03_crear_listas_de_diccionarios/05_Ldict_premios_nobel.py
@@ -1,1 +1,7 @@
 #Trabajar con el diciconario Premios Nobel cuando tiene listas por valores de claves
+
+premios_nobel = {
+    'nombre' : ['Marie Curie', 'Marie Curie','Albert Einstein','Nelson Mandela','Tu Youyou'],
+    'categoria' : ['Fisica','Quimica','Fisica','Paz','Medicina'],
+    'anio' : [1903,1911,1921,1993,2015]
+}
