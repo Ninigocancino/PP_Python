0c952edc04cd6e59ac3ac5d1eb411b5a0b61cc78
VictorCancino <juanvictorcancino@gmail.com>
2025-01-20 16:22:14 -0600

    Se completó la tarea 'crear diccionario anidado con información de planetas'

--- 20_por_ciento/03_conocer_los_diccionarios/05_diccionarios_anidados/05_planteas.py
+++ 20_por_ciento/03_conocer_los_diccionarios/05_diccionarios_anidados/05_planteas.py
@@ -1,3 +1,13 @@
 
 # Trabajar el dicionario 'Planetas' usando anidamiento de diccioanrios
 
+planetas = [
+    {"nombre" : 'Tierra', 'posición' : '3', 'principal_elemento' : 'Oxigeno' },
+    {'nombre' : 'Mercurio', 'posicion' : '1', 'principal_elemento' : 'Hierro'},
+    {'nombre' : 'Jupiter', 'posicion': '5', 'principal_elemento' : 'Hidrogeno'}
+]
+
+print()
+print("Diccionario anidado con información de planetas")
+print(planetas)
+print()
