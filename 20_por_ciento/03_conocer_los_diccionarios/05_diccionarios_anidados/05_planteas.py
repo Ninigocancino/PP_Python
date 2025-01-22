bc8dd948103d6726f8e726a12c8ae5ac08d198ac
VictorCancino <juanvictorcancino@gmail.com>
2025-01-22 16:22:25 -0600

    Se convirtió el diccionario Planetas de una lista de diccionario a un diccionario anidado

--- 20_por_ciento/03_conocer_los_diccionarios/05_diccionarios_anidados/05_planteas.py
+++ 20_por_ciento/03_conocer_los_diccionarios/05_diccionarios_anidados/05_planteas.py
@@ -1,11 +1,28 @@
 
 # Trabajar el dicionario 'Planetas' usando anidamiento de diccioanrios
 
-planetas = [
-    {"nombre" : 'Tierra', 'posición' : '3', 'principal_elemento' : 'Oxigeno' },
-    {'nombre' : 'Mercurio', 'posicion' : '1', 'principal_elemento' : 'Hierro'},
-    {'nombre' : 'Jupiter', 'posicion': '5', 'principal_elemento' : 'Hidrogeno'}
-]
+planetas = {
+    'rocosos' :{
+        'pr01':{
+            "nombre" : 'Tierra',
+            'posición' : '3',
+            'principal_elemento' : 'Oxigeno' },
+        'pr02':{
+            'nombre' : 'Mercurio',
+            'posicion' : '1',
+            'principal_elemento' : 'Hierro'},
+        },
+    'gaseosos':{
+        'pg01' :{
+            'nombre' : 'Jupiter',
+            'posicion': '5',
+            'principal_elemento' : 'Hidrogeno'},
+        'pg02': {
+            'nombre' : 'Saturno',
+            'posicion' : '6',
+            'principal_elemento' : 'Hidrogeno'}
+        }
+    }
 
 print()
 print("Diccionario anidado con información de planetas")
