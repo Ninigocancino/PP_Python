e35f81e9f04ad91eff834e3d0e3737f55005c773
VictorCancino <juanvictorcancino@gmail.com>
2025-01-23 22:44:02 -0600

    Se completó la tarea 'Crear el diciconario animales como lista de diccionarios'

--- 20_por_ciento/03_conocer_los_diccionarios/06_listas_de_diccionarios/02_animales.py
+++ 20_por_ciento/03_conocer_los_diccionarios/06_listas_de_diccionarios/02_animales.py
@@ -1,3 +1,14 @@
 
 #Maneja el diciconario animales como una lista de diccionarios
 
+animales = [
+    {'nombre' : 'perro', 'tipo' : 'dosmetico'},
+    {'nombre': 'gato', 'tipo' : 'domestico'},
+    {'nombre': 'pato', 'tipo' : 'ganado'},
+    {'nombre': 'caiman', 'tipo': 'salvaje'}
+]
+
+print()
+print("Crear el diccioario datos personales como una lista de diccionarios")
+print(animales)
+print("")
