e9f6205b2286d5367ce750bfed2539baf646a378
VictorCancino <juanvictorcancino@gmail.com>
2025-01-23 21:58:37 -0600

    Se completó la tarea 'Crear el diccionario 'datos personales' como una lista de diccionarios'

--- 20_por_ciento/03_conocer_los_diccionarios/06_listas_de_diccionarios/01_datos_personales.py
+++ 20_por_ciento/03_conocer_los_diccionarios/06_listas_de_diccionarios/01_datos_personales.py
@@ -1,2 +1,13 @@
 #Manejar el diccionario datos personales como una lista de diccionarios
 
+datos_personales = [
+    {'nombre': 'Marco', 'edad': '20', 'ciudad': 'Bogota'},
+    {'nombre': 'Alejandra', 'edad':'27','ciudad': 'Tijuana'},
+    {'nombre': 'Alicia', 'edad':'24', 'ciudad': 'Montevideo'},
+    {'nombre': 'Axel', 'edad': '32', 'ciudad': 'Buenos Aires'}
+]
+
+print("")
+print("Crear el diccionario 'datos personales' como una lista de diccionarios")
+print(datos_personales)
+print("")
