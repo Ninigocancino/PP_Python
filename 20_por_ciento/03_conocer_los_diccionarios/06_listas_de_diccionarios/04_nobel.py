659551d13231782c67a0e8095c4b71c999c86ebf
VictorCancino <juanvictorcancino@gmail.com>
2025-01-27 23:35:51 -0600

    Se completó la tarea 'Crear el diciconario 'ganadores_nobel' como una lista de diciconarios'

--- 20_por_ciento/03_conocer_los_diccionarios/06_listas_de_diccionarios/04_nobel.py
+++ 20_por_ciento/03_conocer_los_diccionarios/06_listas_de_diccionarios/04_nobel.py
@@ -1,2 +1,14 @@
 
 #Trabajar con el diccionario 'ganadores_nobel' como una lista de diccionarios
+
+ganadores_nobel = [
+    {'nombre':'Geoffri Hinton','categoria': 'Física','anio':'2024'},
+    {'nombre':'Anne L Huillier','categoria': 'Física','anio':'2023'},
+    {'nombre':'Demis Hassabis','categoria': 'Quimica','anio':'2024'},
+    {'nombre':'Moungi Bawendi','categoria': 'Quimica','anio':'2023'},
+]
+
+print("")
+print("Lista de dicionarios:")
+print(ganadores_nobel)
+print()
