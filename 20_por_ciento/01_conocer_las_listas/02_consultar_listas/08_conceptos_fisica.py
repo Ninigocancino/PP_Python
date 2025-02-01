05e6f237aa7d336c5ffa7cb95a63a93180bacff2
VictorCancino <juanvictorcancino@gmail.com>
2025-02-01 16:39:28 -0600

    Se completó el ejercicio 'concrptos fisica' en la seccción 'consultar listas'

---
+++ 20_por_ciento/01_conocer_las_listas/02_consultar_listas/08_conceptos_fisica.py
@@ -0,0 +1,13 @@
+
+"""
+Instrucciones: Consultar el segundo y el tercer elemento de la lista conceptos_fisica usando dos formas diferentes
+"""
+
+
+conceptos_fisica = [ "Fuerza", "Energía", 'Materia', 'Movimiento', 'Tiempo']
+
+print(conceptos_fisica[2]) #Consulta directa para mostrar en consola
+
+#guardando la consulta en una variable para después imprimir la variable
+elemento = conceptos_fisica[3]
+print(elemento)
