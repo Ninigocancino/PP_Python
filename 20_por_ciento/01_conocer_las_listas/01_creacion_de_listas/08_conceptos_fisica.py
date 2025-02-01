d368686203a3c381dd198b90f8c2fa6c15916a4f
VictorCancino <juanvictorcancino@gmail.com>
2025-02-01 16:32:16 -0600

    Se completó el ejercicio 'conceptos fisica'

---
+++ 20_por_ciento/01_conocer_las_listas/01_creacion_de_listas/08_conceptos_fisica.py
@@ -0,0 +1,9 @@
+
+"""
+Instrucciones: Crear una lista con 5 conceptos indispensables en el campo de la Física
+"""
+
+conceptos_fisica = [ "Fuerza", "Energía", 'Materia', 'Movimiento', 'Tiempo']
+print("")
+print("5 conceptos elementales en la Física: ")
+print(conceptos_fisica)
