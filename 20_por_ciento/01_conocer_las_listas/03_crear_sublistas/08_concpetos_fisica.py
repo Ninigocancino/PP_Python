f054a8e77a9d6d4e0ae69754fe4a9dc636030ceb
VictorCancino <juanvictorcancino@gmail.com>
2025-02-01 16:50:08 -0600

    Se completó el ejercicio 'conceptos fisica' en la sección 'crear sublistas'

---
+++ 20_por_ciento/01_conocer_las_listas/03_crear_sublistas/08_concpetos_fisica.py
@@ -0,0 +1,14 @@
+
+"""
+Instrucciones: Usa la lista conceptos fisica y divide la lista en dos sublistas, luego imprime en consola la lista original y las nuevas sublistas
+"""
+
+
+conceptos_fisica = [ "Fuerza", "Energía", 'Materia', 'Movimiento', 'Tiempo']
+
+sublista = conceptos_fisica[0:3]
+sublista_2 = conceptos_fisica[3:]
+
+print(conceptos_fisica)
+print(sublista)
+print(sublista_2)
