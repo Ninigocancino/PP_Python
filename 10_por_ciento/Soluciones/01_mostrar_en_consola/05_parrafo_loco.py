f492a6681d3363b2958ebdd219f02df5923c8b76
VictorCancino <juanvictorcancino@gmail.com>
2025-02-01 11:29:47 -0600

    Se completó el ejercicio 'frase loca'

---
+++ 10_por_ciento/Soluciones/01_mostrar_en_consola/05_parrafo_loco.py
@@ -0,0 +1,22 @@
+
+"""
+Nombre del ejercicio: Parrafo loco
+
+Instrucciones: Crea un parrafo con un mensaje donde el sujeto, la acción y el tiempo varien.
+"""
+
+txt_1 = "Pepito"
+txt_2 = "enojado"
+txt_3 = 'no había'
+txt_4 = 'comido'
+txt_5 = 'nada'
+txt_6 = 'el recreo'
+txt_7 = 'esperab comer'
+txt_8 = 'porque'
+txt_9 = 'probado bocado'
+txt_10 = 'le gruñia el estomago'
+txt_11 = 'fue a comprar galletas'
+txt_12 = 'comió'
+
+
+print(f"{txt_1} estaba {txt_2} porque ese día {txt_3} {txt_4} {txt_5} en {txt_6}. Hacia mucho tiempo que {txt_7}, {txt_8} casi no habia {txt_9} por lo que {txt_10} asi que finalmente {txt_11} y {txt_12}")
