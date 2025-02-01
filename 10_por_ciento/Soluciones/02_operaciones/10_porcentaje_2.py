9353dc2da16eb2be67366cc6d0f1feb59ec65367
VictorCancino <juanvictorcancino@gmail.com>
2025-02-01 15:57:04 -0600

    Se comletó el ejercicio 'porcentaje 2'

---
+++ 10_por_ciento/Soluciones/02_operaciones/10_porcentaje_2.py
@@ -0,0 +1,26 @@
+"""
+Nombre del ejercicio: Calcular el porcentaje de una cifra, cuando una 
+
+Instrucciones: Crea un programa que calcule un porcentaje determinado, almacenado en una variable, de una cantidad total almacenado en otra variable, luego imprime el resultado con un ensaje descriptivo
+"""
+
+"""
+#el porcentaje de una cantidad dada cuando esta cantidad representa el 100%
+print("el 50 por ciento  de 50 es :")
+cantidad = 100 #representa el 100% 
+
+porcentaje = 50
+
+r =  (cantidad /100) * porcentaje
+#r = r * porcentaje
+
+print(f"el {porcentaje}% de {cantidad} es {r}")
+
+"""
+#Se conoce el resultado y la equivalencia, se necesita descubrir la cantidad que representa el 100%
+#por ejemplo: 20 pesos es el 20% de una cifra desconocida
+cantidad =20
+porcentaje =10
+cantidad_desc = (cantidad / porcentaje) * 100 
+
+print(f"cuando {cantidad} es el {porcentaje}%, {cantidad_desc} es el 100% ")
