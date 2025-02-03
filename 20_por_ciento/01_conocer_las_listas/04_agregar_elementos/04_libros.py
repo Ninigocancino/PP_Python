554441eedc5660c4243c382d96f6c5a0bd2355c3
VictorCancino <juanvictorcancino@gmail.com>
2025-02-02 20:36:11 -0600

    Se completó el ejercicio

---
+++ 20_por_ciento/01_conocer_las_listas/04_agregar_elementos/04_libros.py
@@ -0,0 +1,14 @@
+
+
+libros = ["Hyperfocus","Originals", "Repensar la pobreza","Sapeins"]
+
+print()
+print("Lista original: ")
+print(libros)
+
+otros_libros = ["Esencialismo", "Fuera de serie", "El cisne negro", "Small data", "Así se domina el mundo"]
+
+libros = libros + otros_libros
+print('Agregandos nuevos elementos: ')
+print(libros)
+print()
