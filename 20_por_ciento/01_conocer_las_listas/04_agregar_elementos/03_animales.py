51c7d7dba9fc9386ca190791fff8730dae0c14aa
VictorCancino <juanvictorcancino@gmail.com>
2025-02-02 19:16:08 -0600

    Se completó el ejercicio

---
+++ 20_por_ciento/01_conocer_las_listas/04_agregar_elementos/03_animales.py
@@ -0,0 +1,12 @@
+
+#"Elefante", "Caballo", "Obeja", "Llama", "Cabra"
+
+animales = ["Perro", "Gato", "Tiburón", "Cocodrilo", "León", "Tigre"]
+
+print("Lista original: ")
+print(animales)
+
+animales = animales + ["Elefante"] + ["Caballo"]
+
+print("Agregando elementos: ")
+print(animales)
