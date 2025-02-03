3acfec5aca1fdaabf74f2f2ddd39ce7d125aad1b
VictorCancino <juanvictorcancino@gmail.com>
2025-02-02 18:39:18 -0600

    Se completó el ejercicio

---
+++ 20_por_ciento/01_conocer_las_listas/04_agregar_elementos/02_plantas.py
@@ -0,0 +1,18 @@
+
+#"Cerezo", "Naranjo", "Limonero", "Olivo", "Castaño", "Haya", "Arce", "Abedul", "Cedro","Manzana", "Pera", "Cereza", "Naranja", "Limón", "Banana", "Uva", "Fresa", "Frambuesa", "Mora", "Melón", "Sandía", "Piña", "Mango", "Kiwi","Lechuga", "Tomate", "Pepino", "Zanahoria", "Cebolla", "Pimiento", "Brócoli", "Coliflor", "Espinaca", "Ajo", "Patata", "Calabacín","Rosa", "Lavanda", "Jazmín", "Azalea", "Camelia", "Rododendro", "Hibiscus", "Boj"
+
+plantas = ["Roble", "Pino", "Eucalipto", "Nogal", "Manzano", "Pera" ]
+
+nuevas_plantas = ['Cerezo', 'Limonero','Olivo','Castaño']
+
+plantas_agregadas = plantas + nuevas_plantas
+
+plantas_nuevas = plantas + ['Haya','arce','Abedul']
+
+print()
+print("Lista original: ")
+print(plantas)
+print('Uniendo dos listas')
+print(plantas_agregadas)
+print('Sumando elementos a una listas: ')
+print(plantas_nuevas)
