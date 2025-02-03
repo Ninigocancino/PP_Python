88c5019d64bac22a39a27a71c959ab28fe5be535
VictorCancino <juanvictorcancino@gmail.com>
2025-02-03 12:18:56 -0600

    Se completó el ejercicio

---
+++ 20_por_ciento/01_conocer_las_listas/04_agregar_elementos/08_conceptos_fisica.py
@@ -0,0 +1,10 @@
+
+conceptos_fisica = [ "Fuerza", "Energía", 'Materia', 'Movimiento', 'Tiempo']
+
+print("Lista original: ")
+print(conceptos_fisica)
+
+conceptos_fisica = conceptos_fisica + ['Masa', 'Gravedad', 'Potencia']
+
+print("Lista con nuevos elementos: ")
+print(conceptos_fisica)
