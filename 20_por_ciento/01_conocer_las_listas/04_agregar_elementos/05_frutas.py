c35612f97e466593c076f009175457709c6c3a90
VictorCancino <juanvictorcancino@gmail.com>
2025-02-02 20:47:19 -0600

    Se completó el ejercicio

---
+++ 20_por_ciento/01_conocer_las_listas/04_agregar_elementos/05_frutas.py
@@ -0,0 +1,13 @@
+
+lista_frutas = ['Guanabana','Manzana','Pera','Cereza','Fresa','Uva','Zapote']
+
+print("Lista original: ")
+print(lista_frutas)
+
+frutas = ['Melon','Sandía','Coco','Mango','Pitahaya','Tuna','Tomate','Aguacate','Guayaba','Piña','Papaya','Zapote', 'Kiwi']
+
+mas_frutas = ['Higos','Mamey','Rambutan','Nance','Naranja','Limón','Lima','Platano']
+
+lista_frutas = frutas + mas_frutas
+
+print(lista_frutas)
