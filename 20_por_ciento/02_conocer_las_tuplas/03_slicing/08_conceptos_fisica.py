e2051128a1e185737df1b6122e5ad87b3ab61b40
VictorCancino <juanvictorcancino@gmail.com>
2025-02-03 13:17:10 -0600

    Se completó el ejercicio 'Conceptos física'

---
+++ 20_por_ciento/02_conocer_las_tuplas/03_slicing/08_conceptos_fisica.py
@@ -0,0 +1,9 @@
+
+conceptos_fisica = ('Fuerza','Energía','Materia','Movimiento','Tiempo', 'Masa', 'Gravedad', 'Potencia')
+
+print("Tupla original: ")
+print(conceptos_fisica)
+
+conceptos_fisica = conceptos_fisica[2:6]
+print("Slicing de elementos de la tupla:")
+print(conceptos_fisica)
