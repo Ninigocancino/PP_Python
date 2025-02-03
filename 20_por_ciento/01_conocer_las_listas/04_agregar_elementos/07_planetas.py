60cbdf8c3ab72ba90b0662b73a9b01c5d7bd23d4
VictorCancino <juanvictorcancino@gmail.com>
2025-02-03 11:33:59 -0600

    Se completó el ejercicio

---
+++ 20_por_ciento/01_conocer_las_listas/04_agregar_elementos/07_planetas.py
@@ -0,0 +1,14 @@
+
+planetas = ["Mercurio","Venus","Tierra","Marte","Jupiter", "Urano", "Neptuno"]
+
+print("Lista original: ")
+print(planetas)
+
+otro_planetas = ["Kepler-90b","Kepler-90c","Kepler-90d","Kepler-90e"]
+
+adicionales = ["Kepler-90f","Kepler-90g","Kepler-90h","Kepler-90i"]
+
+planetas = planetas + otro_planetas + adicionales
+
+print("Lista con nuevos elementos agregados: ")
+print(planetas)
