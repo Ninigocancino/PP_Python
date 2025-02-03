70f26654454f45268db27d5ed887d4e8a931893e
VictorCancino <juanvictorcancino@gmail.com>
2025-02-02 20:56:41 -0600

    Se completó el ejercicio

---
+++ 20_por_ciento/01_conocer_las_listas/04_agregar_elementos/06_personajes.py
@@ -0,0 +1,16 @@
+
+personajes = ["Van Gogh", "Miro", "Mone", "Sezane", "Warhol", "Einstein", "Bohr", "Heisenberg", "Curie", "Feynman","Fermi"]
+
+print("Lista original:")
+print(personajes)
+
+personajes_2 = ['Alejandro Magno', 'Julio Cesar', 'Napoleón']
+
+personajes = personajes + personajes_2
+
+print('Lista con más personajes:')
+print(personajes)
+
+personajes = personajes + ['Rey Arturo']
+print("Agregando un ultímo personaje: ")
+print(personajes)
