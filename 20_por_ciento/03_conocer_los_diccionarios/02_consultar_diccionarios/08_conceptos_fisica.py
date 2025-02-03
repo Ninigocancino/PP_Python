0171519008fcd6a0c401b9708c9c333aa784ccc9
VictorCancino <juanvictorcancino@gmail.com>
2025-02-03 16:23:04 -0600

    Se completó el ejercicio

---
+++ 20_por_ciento/03_conocer_los_diccionarios/02_consultar_diccionarios/08_conceptos_fisica.py
@@ -0,0 +1,14 @@
+
+conceptos_fisica = {
+    'Fuerza' : "La fuerza es cualquier interacción que causa que un objeto cambie su estado de movimiento, ya sea que se acelere, desacelere o cambie de dirección. Es una magnitud vectorial, lo que significa que tiene tanto magnitud como dirección.",
+    'Energia' : "La energía es la capacidad de realizar trabajo. Se presenta en diversas formas, como la energía cinética (movimiento), potencial (posición), térmica (calor), eléctrica, etc. La ley de conservación de la energía establece que la energía no se crea ni se destruye, solo se transforma de una forma a otra. ",
+    'Materia' : "La materia es todo aquello que ocupa un lugar en el espacio y tiene masa. Está compuesta por átomos, que a su vez están formados por protones, neutrones y electrones. La materia puede existir en diferentes estados: sólido, líquido, gaseoso y plasma. "
+}
+
+
+print("Accediendo a la clave Materia: ")
+print(conceptos_fisica['Materia'])
+print()
+print("A la clave Energia: ")
+acceder = conceptos_fisica['Energia']
+print(acceder)
