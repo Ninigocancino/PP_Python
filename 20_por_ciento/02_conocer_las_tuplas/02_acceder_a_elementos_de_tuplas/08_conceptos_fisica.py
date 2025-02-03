baeeea2984b800ff8bd39861d655f19fe092b398
VictorCancino <juanvictorcancino@gmail.com>
2025-02-03 13:11:57 -0600

    Se completó el ejercicio

---
+++ 20_por_ciento/02_conocer_las_tuplas/02_acceder_a_elementos_de_tuplas/08_conceptos_fisica.py
@@ -0,0 +1,10 @@
+
+conceptos_fisica = ('Fuerza','Energía','Materia','Movimiento','Tiempo', 'Masa', 'Gravedad', 'Potencia')
+
+print("Tupla original: ")
+print(conceptos_fisica)
+
+conceptos_fisica = conceptos_fisica[1:5]
+
+print("Acceder a valores de tuplas: ")
+print(conceptos_fisica)
