0f4dca3dd289fbd10c3a5c60fc25bdd1be80a028
VictorCancino <juanvictorcancino@gmail.com>
2025-02-05 10:13:41 -0600

    Se completó la tarea 'Agregar un nuevo par clave valor al diccionario' en el ejercicio conceptos de fisica

---
+++ 20_por_ciento/03_conocer_los_diccionarios/03_modificar_diccionarios/08_conceptos_fisica.py
@@ -0,0 +1,11 @@
+
+
+conceptos_fisica = {
+    'Fuerza' : "La fuerza es cualquier interacción que causa que un objeto cambie su estado de movimiento, ya sea que se acelere, desacelere o cambie de dirección. Es una magnitud vectorial, lo que significa que tiene tanto magnitud como dirección.",
+    'Energia' : "La energía es la capacidad de realizar trabajo. Se presenta en diversas formas, como la energía cinética (movimiento), potencial (posición), térmica (calor), eléctrica, etc. La ley de conservación de la energía establece que la energía no se crea ni se destruye, solo se transforma de una forma a otra. ",
+    'Materia' : "La materia es todo aquello que ocupa un lugar en el espacio y tiene masa. Está compuesta por átomos, que a su vez están formados por protones, neutrones y electrones. La materia puede existir en diferentes estados: sólido, líquido, gaseoso y plasma. "
+}
+
+print("Agregar un nuevo par clave-valor")
+conceptos_fisica['Masa']= "La masa es una medida de la cantidad de materia que contiene un objeto. Es una propiedad intrínseca de la materia y se relaciona con la inercia, es decir, la resistencia de un objeto a cambiar su estado de movimiento."
+print(conceptos_fisica)
