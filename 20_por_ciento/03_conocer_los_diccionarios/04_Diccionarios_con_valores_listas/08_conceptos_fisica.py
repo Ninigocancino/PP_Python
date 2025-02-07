4a5accd1c22842c2bd56293ec4b84c15e0bb3b15
VictorCancino <juanvictorcancino@gmail.com>
2025-02-06 23:12:41 -0600

    Se completó la tarea 'crear diccionario conceptos_fisica con listas como valores

---
+++ 20_por_ciento/03_conocer_los_diccionarios/04_Diccionarios_con_valores_listas/08_conceptos_fisica.py
@@ -0,0 +1,10 @@
+
+conceptos_fisica = {
+    'concepto' : ["Fuerza",'Energía','Materia'],
+    'definicion' : ['Interacción que causa que un objeto cambie su estado de movimiento','capacidad de realizar trabajo','todo aquello que ocupa un lugar en el espacio y tiene masa']
+}
+
+print()
+print("Diccionario conceptos de física con listas como valores ")
+print(conceptos_fisica)
+print()
