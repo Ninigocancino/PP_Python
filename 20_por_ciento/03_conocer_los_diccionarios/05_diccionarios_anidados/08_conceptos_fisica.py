3c4f14bc7abbe9d3be8e8eb8984f21e3c6204137
VictorCancino <juanvictorcancino@gmail.com>
2025-02-09 20:39:01 -0600

    Se completó la tarea 'Crear diccionario anidado con cocneptos de fisica'  en el ejercicio 'Conceptos de fisica'

---
+++ 20_por_ciento/03_conocer_los_diccionarios/05_diccionarios_anidados/08_conceptos_fisica.py
@@ -0,0 +1,20 @@
+
+
+conceptos_fisica = {
+    'elementales' : {
+        'fuerza' : "La fuerza es una magnitud física que describe la interacción entre dos objetos, capaz de modificar el estado de movimiento o la forma de un cuerpo.",
+        'energia' : 'La energía es la capacidad que tiene la materia para realizar un trabajo, producir cambios o generar movimiento',
+        'materia' : 'todo lo que podemos tocar, ver, sentir o medir'
+    },
+    'avanzados' : {
+        'avanzados' : {
+            'constante de PLANCK' : 'es un número fundamental en la física que representa el cuanto elemental de acción. Es decir, es la unidad más pequeña de energía que un sistema puede intercambiar.',
+            'entropia' : 'la tendencia natural de los sistemas a pasar de estados ordenados a estados más desordenados',
+            'materia oscura' : 'Es un tipo de materia que no emite, absorbe o refleja luz, lo que la hace invisible a nuestros instrumentos de observación.'
+        }
+    }
+}
+
+print()
+print(conceptos_fisica)
+print()
