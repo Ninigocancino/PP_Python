54e977f56c37f52409c364e234e1ba405ac05106
VictorCancino <juanvictorcancino@gmail.com>
2025-02-11 21:25:06 -0600

    Se completo la tarea 'Crear la  lista de diciconartios Conceptos_fisica'

---
+++ 20_por_ciento/03_conocer_los_diccionarios/06_listas_de_diccionarios/08_conceptos_fisica.py
@@ -0,0 +1,12 @@
+
+conceptos_fisica = [
+    {'Concepto': 'fuerza', 'Definición' : "La fuerza es una magnitud física que describe la interacción entre dos objetos, capaz de modificar el estado de movimiento o la forma de un cuerpo.", 'Tipo': 'Elemental'},
+    {'Concepto': 'energia', 'Definicion' : 'La energía es la capacidad que tiene la materia para realizar un trabajo, producir cambios o generar movimiento','Tipo': 'Elemental'},
+    {'Concepto': 'materia', 'Definicion' : 'todo lo que podemos tocar, ver, sentir o medir', 'Tipo': 'Elemental'},
+    {'Concepto':'constante de PLANCK', 'Definicion' : 'es un número fundamental en la física que representa el cuanto elemental de acción. Es decir, es la unidad más pequeña de energía que un sistema puede intercambiar.', 'Tipo': 'Avanzado'}
+]
+
+print()
+print("Lista de diccionarios 'conceptos_fisica' ")
+print(conceptos_fisica)
+print()
