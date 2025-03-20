94f0e5fd622bb29e111d543520a5326af97dfae7
VictorCancino <juanvictorcancino@gmail.com>
2025-03-19 22:58:04 -0600

    Se agregó la sección 01 del código

---
+++ QUIZ/Fase_1/main.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-  
+"""
+QUIZ ESTÁTICO - FASE 1 (10% dominio Python)
+Propósito: Evaluar conocimientos básicos de primaria en historia, geografía y ciencias
+Audiencia: Niños de 6-12 años
+Característica clave: Retroalimentación inmediata con emojis
+"""
+
+# Sección 01: Declaración de variables iniciales
+puntaje = 0
+
+mensaje_bienvenida = (
+    "\n¡Bienvenido al Quiz de la Escuela Patria México! 🌟\n"
+    "Responde las siguientes 5 preguntas:\n"
+)
