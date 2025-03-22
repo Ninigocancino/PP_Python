ea223a8445e34607084f50163020c7bd63faae5d
VictorCancino <juanvictorcancino@gmail.com>
2025-03-21 19:57:51 -0600

    Se agregó el alcance del ejercicio

---
+++ QUIZ/Fase_2/main.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""
+Fase 2 - Quiz dinámico 
+Novedades:
+-Preguntas cargadas desde una lista de diccionarios
+-Sistema escalable (añadir preguntas sin modificar lógica)
+-Lectura de archivos csv (requiere archivo prentas.csv)
+"""
