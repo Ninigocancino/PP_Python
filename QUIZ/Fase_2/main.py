f2b2d604fa3730875e677b308c819b44431959a0
VictorCancino <juanvictorcancino@gmail.com>
2025-03-24 22:07:26 -0600

    Se agregó la sección de preguntas al código

--- QUIZ/Fase_2/main.py
+++ QUIZ/Fase_2/main.py
@@ -8,4 +8,12 @@ Novedades:
 -Sistema escalable (añadir preguntas sin facilmente)
 """
 
-#
+# Sección 1: Lista de preguntas 
+preguntas = [
+    ["\n1. ¿Qué celebra México el 16 de septiembre?", "independencia"],
+    ["\n2. ¿Cuál es la capital de México", "ciudad de méxico"],
+    ["\n3. ¿Cuál es el animal nacional de México", "águila real"],
+    ["\n4. ¿Cuál es el color representativo de México", "rosa mexicano"],
+    ["\n5. ¿Cuál es la comida más conocida de Méxio", "tacos"]
+]
+
