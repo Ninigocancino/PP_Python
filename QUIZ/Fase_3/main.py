4714a9944a5e7018490ee211571b4f4d0182e032
VictorCancino <juanvictorcancino@gmail.com>
2025-03-25 20:32:33 -0600

    Se agregarón los diccionariós que contienen las preguntas, respuestas y pistas del quiz

--- QUIZ/Fase_3/main.py
+++ QUIZ/Fase_3/main.py
@@ -7,3 +7,18 @@ Requerimeintos:
 - Sistema de estadísticas por tema
 - Validación flexible de respuestas
 """
+
+# Sección 1: Estructura de diccionarios anidados
+
+preguntas = [
+    {
+        "texto" : "\n1. ¿Qué celebra México el 16 de septiembre?",
+        "respuesta" : {"opciones": ["independencia"], "tema": "Historia"},
+        "pistas" : ["¡Es un evento patrio!", "Comenzo en 1810"]
+    },
+    {
+        "texto" : "\n2. ¿Cuál es la capital de México?",
+        "respuesta" : {"opciones": ["ciudad de méxico", "cdmx"], "tema": "Geografía"},
+        "pistas" : ["Tiene un zócalo famoso", "Es la ciudad más grande del país"]
+    }
+]
