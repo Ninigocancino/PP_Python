16359fae1dc64cd678956ba48ceb157db9e69206
VictorCancino <juanvictorcancino@gmail.com>
2025-03-25 20:19:40 -0600

    Se agregarón la descripción general del programa y sus requerimientos

---
+++ QUIZ/Fase_3/main.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""
+Fase 3 - Quiz con estadísticas
+Requerimeintos:
+- Preguntas almacenadas en diccionarios con categorías
+- Sistema de estadísticas por tema
+- Validación flexible de respuestas
+"""
