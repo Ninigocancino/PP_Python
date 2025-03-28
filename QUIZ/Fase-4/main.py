3f9078316de8614400e9447cf93596d7581a101d
VictorCancino <juanvictorcancino@gmail.com>
2025-03-28 17:38:40 -0600

    Se inicio con la sección 1 del programa 'Creación de funciones principales' y se creó la 'función cargar_preguntas'

--- QUIZ/Fase-4/main.py
+++ QUIZ/Fase-4/main.py
@@ -8,3 +8,16 @@ Requisitos:
 -Sistema de reintentos
 """
 
+# Sección 1: Creación de funciones principales
+
+def cargar_preguntas():
+    return [
+
+        #Retorna preguntas desde una lista
+        
+        {
+            "texto" : "\n1. ¿Qué celebra México el 16 de septiembre?",
+            "respuestas" : {"opciones": ["independencia"], "tema": "Historia"},
+            "pistas" : ["¡Es un evento patrio!", "Comenzó en 1810"]
+        },
+    ]
