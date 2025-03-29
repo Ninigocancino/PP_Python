aab9e8306851aaa5849d4dbcc086a539c6e863c3
VictorCancino <juanvictorcancino@gmail.com>
2025-03-28 18:11:12 -0600

    Se creó la función 'obtener_resuesta_usuario'

--- QUIZ/Fase-4/main.py
+++ QUIZ/Fase-4/main.py
@@ -14,10 +14,24 @@ def cargar_preguntas():
     return [
 
         #Retorna preguntas desde una lista
-        
+
         {
             "texto" : "\n1. ¿Qué celebra México el 16 de septiembre?",
             "respuestas" : {"opciones": ["independencia"], "tema": "Historia"},
             "pistas" : ["¡Es un evento patrio!", "Comenzó en 1810"]
         },
     ]
+
+def obtener_respuesta_usuario(pregunta_texto):
+
+    #Captura y valida la respuesta del usuario con manejo de errores
+
+    while True: 
+        try:
+            respuesta = input(pregunta_texto).lower().strip()
+            return respuesta 
+        except KeyboardInterrupt:
+            print("\n ⚠️ ¡Has salido del quiz!")
+            exit()
+        except:
+            print("Error inesperado. Intenta nuevamente.")
